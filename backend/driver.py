74366552b076014791b50b25c56450b90c9ecbc3
Faris Durrani <afarisdurrani@gmail.com>
2022-05-13 06:24:30 +0800

    Minor: file reformat

--- backend/driver.py
+++ backend/driver.py
@@ -34,45 +34,46 @@ def drive(user_arch: list[str], criterion: str, optimizer_name: str,         pro
     """
     try:
         if (default):
-            #If the user specifies no dataset, use iris as the default
+            # If the user specifies no dataset, use iris as the default
             dataset = load_iris()
             input_df = pd.DataFrame(dataset.data)
-            input_df['class']=dataset.target
-            input_df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
-            input_df.dropna(how="all", inplace=True) # remove any empty lines
+            input_df['class'] = dataset.target
+            input_df.columns = ['sepal_len', 'sepal_wid',
+                                'petal_len', 'petal_wid', 'class']
+            input_df.dropna(how="all", inplace=True)  # remove any empty lines
             y = input_df["class"]
             X = input_df.drop("class", axis=1, inplace=False)
         else:
             input_df = pd.read_csv(CSV_FILE_NAME)
             y = input_df[target]
             X = input_df.drop(target, axis=1, inplace=False)
-        
-        #Convert to tensor
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0, shuffle=shuffle)
-        X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor = get_tensors(X_train, X_test, y_train, y_test)
-        
-        
-        #Build the Deep Learning model that the user wants
+
+        # Convert to tensor
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=test_size, random_state=0, shuffle=shuffle)
+        X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor = get_tensors(
+            X_train, X_test, y_train, y_test)
+
+        # Build the Deep Learning model that the user wants
         model = DLModel(parse_user_architecture(user_arch))
         print(f"model: {model}")
-        optimizer = get_optimizer(model, optimizer_name=optimizer_name, learning_rate=0.05)
+        optimizer = get_optimizer(
+            model, optimizer_name=optimizer_name, learning_rate=0.05)
         criterion = LossFunctions.get_loss_obj(LossFunctions[criterion])
         print(f"loss criterion: {criterion}")
-        train_loader, test_loader = get_dataloaders(X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor, batch_size=20)
-        train_loss, test_loss = train_model(model, train_loader, test_loader, optimizer, criterion, epochs, problem_type)
+        train_loader, test_loader = get_dataloaders(
+            X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor, batch_size=20)
+        train_loss, test_loss = train_model(
+            model, train_loader, test_loader, optimizer, criterion, epochs, problem_type)
         print(f"train loss: {train_loss}")
         print(f"test loss: {test_loss}")
         pred, ground_truth = get_predictions(model, test_loader)
         generate_loss_plot(train_loss, test_loss)
-        
+
     except Exception:
-        return traceback.format_exc() #give exception in string format
+        return traceback.format_exc()  # give exception in string format
 
 
 if __name__ == "__main__":
-    print(drive(["nn.Linear(4, 10)", "nn.Linear(10, 3)"], "CELOSS", "SGD", problem_type="classification", default=True, epochs=10))
-    
-    
-    
-    
-    
+    print(drive(["nn.Linear(4, 10)", "nn.Linear(10, 3)"], "CELOSS",
+          "SGD", problem_type="classification", default=True, epochs=10))

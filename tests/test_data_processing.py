e8e38dcd6c375b25b77fc3addbd3fb5b4fe85b60
ksubramanian40 <ksubramanian40@gatech.edu>
2022-05-13 18:04:46 -0400

    backend tests

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,25 +1,14 @@
 #unit tests for dataset.py file (maybe dummy csv and see if it works properly. Assert 2 files the same)
 
+from urllib.error import URLError
 import pytest
 import csv
 from backend.constants import *
 from backend.dataset import *
-import filecmp
-import os
 
 @pytest.mark.parametrize('url,path_to_file', [('https://raw.githubusercontent.com/karkir0003/dummy/main/job.csv','./tests/expected/job.csv'), ('https://raw.githubusercontent.com/karkir0003/dummy/main/cars.csv', './tests/expected/cars.csv')])
 def test_dataset_url_reading(url, path_to_file):
     read_dataset(url)
-    res = filecmp.cmp(CSV_FILE_PATH, path_to_file, shallow=False)
-    #print(res)
-    
-    # with open(CSV_FILE_PATH, mode="r") as f1:
-    #     reader1 = csv.reader(f1)
-    # with open(path_to_file, mode="r") as f2:
-    #     reader2 = csv.reader(f2)
-    #     #assert len(reader1) == len(reader2) #same number of lines
-    #     for line1, line2 in enumerate(zip(reader1, reader2)):
-    #         assert line1 == line2
     
     with open(CSV_FILE_PATH) as f1:
         with open(path_to_file) as f2:
@@ -27,6 +16,8 @@ def test_dataset_url_reading(url, path_to_file):
             reader2 = csv.reader(f2)
             assert [line for line in reader1][:-1] == [line for line in reader2]
 
-@pytest.raises()
-def test_dataset_invalid_url(url, path_to_file)
+@pytest.mark.parametrize("url,path_to_file", [("https://csv.com/jobs.csv", './tests/expected/jobs.csv'), ("https://csv.com/cars.csv", "./tests/expected/cars.csv")])
+def test_dataset_invalid_url(url, path_to_file):
+    with pytest.raises(Exception):
+        read_dataset(url)
     

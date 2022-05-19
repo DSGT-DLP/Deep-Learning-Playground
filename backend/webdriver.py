8e5292df3fe23951a83529cda50f0e9930f9fad1
karkir0003 <54720987+karkir0003@users.noreply.github.com>
2022-05-18 22:52:57 -0700

    Add files via upload

--- backend/webdriver.py
+++ backend/webdriver.py
@@ -3,22 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from constants import ONNX_MODEL, OPEN_FILE_BUTTON, NETRON_URL
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(
+    executable_path="C:/Users/karki/Downloads/chromedriver_win32/chromedriver.exe",
+    options=options,
+)
+driver.get(NETRON_URL)
 
-def open_onnx_file(onnx_model):
+
+def open_onnx_file():
     """
     Helper function that uses selenium webdriver to open
     an onnx file containing the trained model
     Args:
         onnx_model (file name/path): path to onnx model
     """
-    print("opening chrome")
-    driver = webdriver.Chrome(
-        "C:/Users/karki/Downloads/chromedriver_win32/chromedriver.exe"
-    )
-    print("going to netron.app")
-    driver.get(NETRON_URL)
-    print("uploading onnx model")
-    element = driver.find_element_by_xpath("//input[@type='file']")
-    driver.implicitly_wait(200)
+    element = driver.find_element_by_xpath("//*[@id='open-file-button']")
+    driver.implicitly_wait(5)
+    element.click()
     # element = driver.find_element_by_id(OPEN_FILE_BUTTON).click()
-    element.send_keys(os.path.abspath(ONNX_MODEL))
+    full_path = str(os.path.abspath(ONNX_MODEL))
+    print(os.path.abspath(ONNX_MODEL))
+    driver.implicitly_wait(5)
+    element.send_keys(full_path)
+    print("upload success")
+    # element.click()
+
+

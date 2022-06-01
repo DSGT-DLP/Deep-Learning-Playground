f36251ec9ea78c581c9b16eb2a3e98e91ef435fc
elawton612 <102317424+elawton612@users.noreply.github.com>
2022-06-01 02:45:24 -0400

    recommit of navbar changes

--- frontend/playground-frontend/src/components/Title.js
+++ frontend/playground-frontend/src/components/Title.js
@@ -3,20 +3,25 @@ import Navbar from "./Navbar/Navbar";
 import { COLORS, GENERAL_STYLES, LAYOUT } from "../constants";
 const Title = () => {
   return (
-    <h1 style={styles.h1}>
+    <div style={styles.h1}>
       <img src={DSGTLogo} alt="DSGT Logo" width="60" height="60" />
       Deep Learning Playground
-      <Navbar />
-    </h1>
+      <div style={styles.nav}>
+        <Navbar />
+      </div>
+    </div>
   );
 };
 export default Title;
 const styles = {
   h1: {
     ...GENERAL_STYLES.p,
-    padding: 10,
     margin: "0 0 0px 0",
     display: "flex",
     alignItems: "center",
+    fontSize: 30,
+  },
+  nav: {
+    float: "right",
   },
 };

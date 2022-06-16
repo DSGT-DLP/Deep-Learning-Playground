09fb75b392a75bddf377c105423413d48f3f834b
anikethswain <swainaniketh@gmail.com>
2022-06-15 22:19:45 -0400

    Fixed images placement within about page
    Signed-off-by: anikethswain <swainaniketh@gmail.com>

---
+++ frontend/playground-frontend/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import DSGTLogo from "../../images/logos/dsgt-logo-light.png";
+
+const Footer = () => {
+    
+}

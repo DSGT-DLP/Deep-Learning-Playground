f503b1eb36bb3a575cfe37aeee7d3c6da7f4fc4b
Caicedo, Juan Diego <jrealpe3@gatech.edu>
2022-06-19 14:32:18 -0400

    included the donate buttom in website

--- frontend/playground-frontend/src/components/Navbar/Navbar.js
+++ frontend/playground-frontend/src/components/Navbar/Navbar.js
@@ -7,34 +7,62 @@ import { FaYoutube } from "react-icons/fa";
 import { LAYOUT } from "../../constants";
 
 const Navbar = () => {
-    return (
-        <div id="nav-bar">
-            <div className="image-title" style={{ fontSize: "25px" }}>
-                <img src={DSGTLogo} alt="DSGT Logo" width="61" height="60" />
-                <div style={{ marginRight: 10 }} />
-                Deep Learning Playground
-            </div>
-            <ul className="nav">
-                <li id="navElement">
-                    <a href="https://github.com/karkir0003/Deep-Learning-Playground">
-                        <FaGithub size="60" />
-                    </a>
-                </li>
-                <li id="title-name"></li>
-                <li className="navElement">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="navElement">
-                    <Link to="/About">About</Link>
-                </li>
-                <li className="navElement">
-                    <Link to="/Wiki">Wiki</Link>
-                </li>
-                <li className="navElement">
-                    <Link to="/Feedback">Feedback </Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div id="nav-bar">
+      <div className="image-title" style={{ fontSize: "25px" }}>
+        <img src={DSGTLogo} alt="DSGT Logo" width="61" height="60" />
+        <div style={{ marginRight: 10 }} />
+        Deep Learning Playground
+      </div>
+      <ul className="nav">
+        <li id="navElement">
+          <a href="https://github.com/karkir0003/Deep-Learning-Playground">
+            <FaGithub size="60" />
+          </a>
+        </li>
+        <li id="title-name"></li>
+        <li className="navElement">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navElement">
+          <Link to="/About">About</Link>
+        </li>
+        <li className="navElement">
+          <Link to="/Wiki">Wiki</Link>
+        </li>
+        <li className="navElement">
+          <Link to="/Feedback">Feedback </Link>
+        </li>
+        <li className="navElement">
+          <form
+            action="https://www.paypal.com/donate"
+            method="post"
+            target="_top"
+          >
+            <input
+              type="hidden"
+              name="hosted_button_id"
+              value="Y7VZR7PVRLTPE"
+            />
+            <input
+              type="image"
+              src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+              border="0"
+              name="submit"
+              title="PayPal - The safer, easier way to pay online!"
+              alt="Donate with PayPal button"
+            />
+            <img
+              alt=""
+              border="0"
+              src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+              width="1"
+              height="1"
+            />
+          </form>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Navbar;

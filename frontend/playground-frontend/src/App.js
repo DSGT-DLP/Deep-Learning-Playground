3a8696cd94685fb543ef47feea0ce55cbef204b9
ejlawton612 <106289134+ejlawton612@users.noreply.github.com>
2022-06-23 00:48:27 -0400

    Adding google analytics

--- frontend/playground-frontend/src/App.js
+++ frontend/playground-frontend/src/App.js
@@ -3,22 +3,32 @@ import Title from "./components/Title";
 import Home from "./Home";
 import About from "./components/About/About";
 import Wiki from "./components/Wiki/Wiki";
-import Feedback from "./components/FeedBackForm";
 import "./App.css";
+import ReactGA from "react-ga";
+import RouteChangeTracker from "./components/RouteChangeTracker";
+
+const TRACKING_ID = "UA-232631903-1";
+ReactGA.initialize(TRACKING_ID, { testMode: process.env.NODE_ENV === "test" });
+
+ReactGA.exception({
+  description: "An error ocurred",
+  fatal: true,
+});
 
 function App() {
   return (
     <div>
       <Router>
         <Title />
+        <RouteChangeTracker />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Wiki" element={<Wiki />} />
-          <Route path="/feedback" element={<Feedback />} />
         </Routes>
       </Router>
     </div>
   );
 }
 export default App;
+

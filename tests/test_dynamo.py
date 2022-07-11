8f10c2e54446c637807c8bef8f4ced87016465fc
Samarth Chandna <samarthchandna52@gmail.com>
2022-07-11 15:08:37 +0400

    added helper functions to create StatusData objects and add valid records to DynamoDB table

--- tests/test_dynamo.py
+++ tests/test_dynamo.py
@@ -16,8 +16,19 @@ status_ddb.delete_status("dummy",StatusEnum.SUCCESS.name)
 
 """
 
+def create_status_data(id: str, status: str, timestamp: str, teardown: list[str]) -> StatusData:
+    '''
+    Helper method to create a StatuData object from input params
+    '''
+    dummy_status = {"request_id": id, "status": status, "timestamp": timestamp}
+    status_data = StatusData(set_status_data(dummy_status, StatusAttribute.REQUEST_ID),
+                                set_status_data(dummy_status, StatusAttribute.STATUS),
+                                set_status_data(dummy_status, StatusAttribute.TIMESTAMP))
+    teardown.append(id)
+    return status_data
+
 @pytest.fixture
-def teardown():
+def teardown() -> None:
     '''
     Deletes any records added by the test cases.
     '''
@@ -32,6 +43,7 @@ def teardown():
                 status_ddb.delete_status(id, StatusEnum.SUCCESS.name)
             except:
                 pass
+
 
 @pytest.mark.parametrize(
     "id,status,timestamp",
@@ -58,64 +70,57 @@ def teardown():
         )        
     ],
 )
-def test_status_entry(id,status,timestamp, teardown):
+def test_status_entry(id, status, timestamp, teardown):
     status_ddb = get_status_table("us-west-2")
-    dummy_status = {"request_id": id, "status": status, "timestamp": timestamp}
-    status_data = StatusData(set_status_data(dummy_status, StatusAttribute.REQUEST_ID),
-                                set_status_data(dummy_status, StatusAttribute.STATUS),
-                                set_status_data(dummy_status, StatusAttribute.TIMESTAMP))
-    teardown.append(id)
+    status_data = create_status_data(id, status, timestamp, teardown)
     output = status_ddb.create_status_entry(status_data)
     assert output == "Success"
+
 
 @pytest.mark.parametrize(
     "id,status,timestamp",
     [
         (
-            None,
+            None,                                      # missing id
             StatusEnum.SUCCESS.name,
             datetime.datetime.now().isoformat()
         ),
         (
             "2",
-            None,
+            None,                                      # missing status
             datetime.datetime.now().isoformat()
         ),
         (
             "3",
             StatusEnum.SUCCESS.name,
-            None
+            None                                       # missing timestamp
         ),
         (
-            "dummy",
+            "dummy",                                   # invalid id, should only be numbers in string format (i think)
             StatusEnum.SUCCESS.name,
             datetime.datetime.now().isoformat()
         ),
         (
-            5,
+            5,                                         # invalid id type (should be string)
             StatusEnum.SUCCESS.name,
             datetime.datetime.now().isoformat()
         ),
         (
             "6",
-            "random status string",
+            "random status string",                    # invalid status type (should be one of the elements of StatusEnum)
             datetime.datetime.now().isoformat()
         )
     ]
 )
-def test_status_entry_invalid_param(id,status,timestamp, teardown):
+def test_status_entry_invalid_param(id, status, timestamp, teardown):
     status_ddb = get_status_table("us-west-2")
-    dummy_status = {"request_id": id, "status": status, "timestamp": timestamp}
-    status_data = StatusData(set_status_data(dummy_status, StatusAttribute.REQUEST_ID),
-                                set_status_data(dummy_status, StatusAttribute.STATUS),
-                                set_status_data(dummy_status, StatusAttribute.TIMESTAMP))
+    status_data = create_status_data(id, status, timestamp, teardown)
     with pytest.raises(ValueError) as err:
-        teardown.append(id)
         status_ddb.create_status_entry(status_data)
     
- 
+
 @pytest.mark.parametrize(
-    "id,status,time_stamp",
+    "id,status,timestamp",
     [
         (
             "1",
@@ -124,23 +129,23 @@ def test_status_entry_invalid_param(id,status,timestamp, teardown):
         ),
     ],
 )    
-def test_status_entry_duplicates(id, status, time_stamp, teardown):
+def test_status_entry_duplicates(id, status, timestamp, teardown):
     status_ddb = get_status_table("us-west-2")
-    dummy_status1 = {"request_id": id, "status": status, "timestamp": time_stamp}
-    status_data1 = StatusData(set_status_data(dummy_status1, StatusAttribute.REQUEST_ID),
-                                set_status_data(dummy_status1, StatusAttribute.STATUS),
-                                set_status_data(dummy_status1, StatusAttribute.TIMESTAMP))
-    teardown.append(id)
+    status_data1 = create_status_data(id, status, timestamp, teardown)
     output1 = status_ddb.create_status_entry(status_data1)
     assert output1 == "Success"
     
-    dummy_status2 = {"request_id": id, "status": status, "timestamp": datetime.datetime.now().isoformat()}
-    status_data2 = StatusData(set_status_data(dummy_status2, StatusAttribute.REQUEST_ID),
-                                set_status_data(dummy_status2, StatusAttribute.STATUS),
-                                set_status_data(dummy_status2, StatusAttribute.TIMESTAMP))
-    
-    
+    # If request_id already exists, it should not be altered by create_status_entry
+    status_data2 = create_status_data(id, status, timestamp, teardown)    
     with pytest.raises(ValueError) as err:
-        # If request_id already exists, it should not be altered by create_status_entry
         status_ddb.create_status_entry(status_data2)
     status_ddb.delete_status(id, StatusEnum.SUCCESS.name)
+    
+
+
+def valid_status_entry_helper(id: str, status: str, timestamp: str, teardown: list[str], status_ddb: StatusDDBUtil) -> None:
+    """
+    Helper method to temporarily add records in DynamoDB table to test get_record, update_status, and delete_status functions
+    """
+    status_data = create_status_data(id, status, timestamp, teardown)
+    status_ddb.create_status_entry(status_data)

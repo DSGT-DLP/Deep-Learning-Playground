735ea2587b6e1a91781740dcf9382a0e3bc72b56
andrewpeng02 <andrewpeng02@gmail.com>
2022-08-07 16:52:35 -0500

    add firebase config

--- frontend/playground-frontend/src/index.js
+++ frontend/playground-frontend/src/index.js
@@ -1,6 +1,20 @@
+import { initializeApp } from "firebase/app";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMq0RmoCB1G6JHwwAvVXdcaykAhvwfv4Q",
+  authDomain: "dsgt-authentication.firebaseapp.com",
+  projectId: "dsgt-authentication",
+  storageBucket: "dsgt-authentication.appspot.com",
+  messagingSenderId: "15938712141",
+  appId: "1:15938712141:web:0ecd946393c0584e72a204",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

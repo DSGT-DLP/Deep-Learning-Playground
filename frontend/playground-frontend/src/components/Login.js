e2657cd99c2621951dd7f7d4376b7c80cac55b1d
avayedawadi <avayedawadi@users.noreply.github.com>
2022-08-08 17:15:53 +0000

    Prettified Code!

--- frontend/playground-frontend/src/components/Login.js
+++ frontend/playground-frontend/src/components/Login.js
@@ -3,20 +3,20 @@ import React from "react";
 const Login = () => {
   return (
     <div className="form">
-     <form>
-       <div className="input-container">
-         <label>Username </label>
-         <input type="text" name="uname" required />
-       </div>
-       <div className="input-container">
-         <label>Password </label>
-         <input type="password" name="pass" required />
-       </div>
-       <div className="button-container">
-         <input type="submit" />
-       </div>
-     </form>
-   </div>
+      <form>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
   );
 };
 

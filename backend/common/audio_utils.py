0f4afaf2d4f6442ef91f7a4ebe8ec01f70c7305b
Samarth Chandna <samarthchandna52@gmail.com>
2022-08-17 14:33:21 -0400

    added transform to standardize dataset sample_rate and did minor refactoring

--- backend/common/audio_utils.py
+++ backend/common/audio_utils.py
@@ -9,17 +9,17 @@ dataset_labels = {
     )}
 }
 
-def make_collate_fn(dataset_name, sample_rate, new_sample_rate, max_sec):
+def make_collate_fn(dataset_name, sample_rate, max_sec):
     def make_collate_fn(batch):
         max_len = int(sample_rate * max_sec)
         aud_tensors = []
         labels = []
         label_to_index = dataset_labels[dataset_name]
-        resampler = T.Resample(sample_rate, new_sample_rate)
         
         for aud_tensor, _, label, *_ in batch:
-            aud_tensors.append(equate_tensor_len(aud_tensor, max_len))
-            aud_tensors[-1] = resampler(aud_tensors[-1])
+            transformed_aud_tensor = T.Resample(aud_tensor.shape[1], sample_rate)(aud_tensor)
+            transformed_aud_tensor = equate_tensor_len(aud_tensor, max_len)
+            aud_tensors.append(transformed_aud_tensor)
             labels.append(label_to_index[label])
             
         aud_tensors = torch.stack(tuple(aud_tensors))
@@ -28,16 +28,18 @@ def make_collate_fn(dataset_name, sample_rate, new_sample_rate, max_sec):
     return make_collate_fn
 
 def equate_tensor_len(tensor, max_len):
-    num_channels, signal_len = tensor.shape
+    signal_len = tensor.shape[1]
     if (signal_len < max_len):
-        tensor = pad_audio_tensor(tensor, num_channels, signal_len, max_len)
+        new_tensor = pad_audio_tensor(tensor, max_len)
     elif (signal_len > max_len):
-        tensor = tensor[:,:max_len]
-    return tensor
+        new_tensor = tensor[:,:max_len]
+    else:
+        new_tensor = tensor
+    return new_tensor
         
-def pad_audio_tensor(tensor, num_channels, signal_len, new_len):
+def pad_audio_tensor(tensor, new_len):
+    num_channels, signal_len = tensor.shape
     pad_len = new_len - signal_len
-    print(pad_len)
     pad_start_len = random.randint(0, pad_len)
     pad_end_len = pad_len - pad_start_len
     
@@ -60,12 +62,15 @@ if __name__=='__main__':
         raise ValueError('DLP currently supports only single channel datasets')
     train_loader = torch.utils.data.DataLoader(
         train_set,
-        batch_size=256,
-        collate_fn=make_collate_fn('SPEECHCOMMANDS', 16000, 32000, 0.9),
+        batch_size=41,
+        collate_fn=make_collate_fn('SPEECHCOMMANDS', 16000, 0.9),
         shuffle=False
     )
     
-    print(next(iter(train_loader)))
+    batch = next(iter(train_loader))
+    print(batch)
+    # print(train_set[0][0])
+    # print(train_set[0][0].unsqueeze(0))
     # print(train_set[39], train_set[39][0].shape)
     # print(train_set[40], train_set[40][0].shape)
     # batch = [train_set[i] for i in range(40)]

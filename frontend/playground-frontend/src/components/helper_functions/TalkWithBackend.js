956deef6b25de6d8a4153ed622a0deb3d94f340e
karkir0003 <karkir0003@gmail.com>
2022-08-29 09:22:51 -0400

    fix for prod to work

--- frontend/playground-frontend/src/components/helper_functions/TalkWithBackend.js
+++ frontend/playground-frontend/src/components/helper_functions/TalkWithBackend.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { toast } from "react-toastify";
 
-const socket = io(":8000");
+const socket = io("");
 socket.on("connect", () => {
   frontendLog(`connected to socket`);
 });

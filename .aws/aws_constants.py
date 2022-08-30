05f48e20e674bc93042c456f1a1f98673fbe49b8
Faris Durrani <afarisdurrani@gmail.com>
2022-08-30 09:45:55 -0400

    Added AWS_REGION

--- .aws/aws_constants.py
+++ .aws/aws_constants.py
@@ -1,5 +1,8 @@
-import os 
 #.env file constants
+import os
+import sys
+sys.path.append(os.path.dirname(os.getcwd()))
+from backend.common.constants import AWS_REGION
 
 ENV_KEYS = ["REACT_APP_SECRET_KEY",
             "REACT_APP_CAPTCHA_SITE_KEY", "REACT_APP_FEEDBACK_EMAIL"]

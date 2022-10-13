e52a4aeb05a71ff74fe08131d70a71feda70ebfb
Samarth Chandna <samarthchandna52@gmail.com>
2022-10-12 23:22:27 -0400

    added helper functions to update execution-db table with progress and status

---
+++ backend/common/execution_updates.py
@@ -0,0 +1,38 @@
+import datetime
+import threading
+
+from backend.aws_helpers.dynamo_db_utils.execution_db import get_execution_table, ExecutionData
+
+execution_dict = {}
+
+def get_execution_dict():
+    return execution_dict
+
+def regular_updates(execution_id):
+    execution_db = get_execution_table()
+    def update():
+        if execution_id in execution_dict:
+            execution_db.update_record(execution_id, progress=execution_dict[execution_id])
+            threading.Timer(30, update).start()
+    update()
+
+def create_execution(execution_id, user_id, name, data_source):
+    execution_db = get_execution_table()
+    record = ExecutionData(execution_id, user_id, name, datetime.datetime.now().isoformat(), data_source, 'STARTING', 0)
+    execution_db.create_record(record)
+
+def initialize_training(execution_id):
+    execution_db = get_execution_table()
+    execution_db.update_record(execution_id, status='TRAINING')
+
+def end_training(execution_id, success: bool):
+    execution_db = get_execution_table()
+    status = 'SUCCESS' if success else 'ERROR'
+    execution_db.update_record(execution_id, status=status, progress=100)
+    execution_dict.pop(execution_id)
+
+def end_training_success(execution_id):
+    end_training(execution_id, True)
+    
+def end_training_failure(execution_id):
+    end_training(execution_id, False)

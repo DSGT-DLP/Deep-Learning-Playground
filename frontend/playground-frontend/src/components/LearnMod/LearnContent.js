bb7959da1f58a6f6902764f5a264e118022fc81e
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2022-10-30 21:48:20 -0400

    Revert "Added navigation between module sections"
    This reverts commit 933e8bc4c749149be2c4e8902d09a31189b7c7f2.

--- frontend/playground-frontend/src/components/LearnMod/LearnContent.js
+++ frontend/playground-frontend/src/components/LearnMod/LearnContent.js
@@ -7,7 +7,7 @@ const LearnContent = () => {
     
     let moduleContent = location.state;
     
-    const [subSection, setSubSection] = useState(0);
+    const [subSection] = useState(0);
 
     return (
         <>
@@ -15,7 +15,6 @@ const LearnContent = () => {
             <h1 className="headers">{moduleContent.title}</h1>
       </div>
       <div id="learningContentDiv">
-            <h2>{moduleContent.subClasses[subSection].title}</h2>
           {
             moduleContent.subClasses[subSection].content.map((contentComponent, index) => {
 
@@ -29,10 +28,6 @@ const LearnContent = () => {
 
             })
           }
-      </div>
-      <div>
-        <button onClick={() => {if (subSection > 0) {setSubSection(subSection - 1);}}}>Previous</button>
-        <button onClick={() => {if (subSection < moduleContent.subClasses.length - 1) {setSubSection(subSection + 1);}}}>Next</button>
       </div>   
         </>
     );

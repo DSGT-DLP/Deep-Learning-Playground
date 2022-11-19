feda95e6ccb336bc8bd5c928ca36167c3ac794fa
karkir0003 <karkir0003@gmail.com>
2022-11-18 21:44:04 -0500

    s3 constants

---
+++ backend/aws_helpers/s3_utils/s3_bucket_names.py
@@ -0,0 +1,2 @@
+FILE_UPLOAD_BUCKET_NAME = ""  #put name of S3 bucket we create for file upload purposes
+EXECUTION_BUCKET_NAME = "" #name of s3 bucket we use for storing execution data for user training requests

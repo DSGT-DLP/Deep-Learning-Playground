fbec11ae20f8bfa65dca45a578aa9740ff751a40
Thomas Chin <thomas081904@gmail.com>
2022-12-21 22:56:18 +0800

    Created execution table terraform

--- dlp-terraform/dlp.tf
+++ dlp-terraform/dlp.tf
@@ -43,27 +43,42 @@ resource "aws_s3_bucket_lifecycle_configuration" "upload_bucket_config" {
     expiration {
       days = 5
     }
-    id = "logs"
+    id     = "logs"
     status = "Enabled"
   }
 }
-resource "aws_dynamodb_table" "dlp-execution-db-bucket" { 
-   name = "dlp-execution-db-bucket" 
-   attribute { 
-      name = "" 
-      type = "S" 
-   } 
-   ttl { 
-     enabled = true
-     attribute_name = "expiryPeriod"  
-   }
-   point_in_time_recovery { enabled = true } 
-   server_side_encryption { enabled = true } 
-   
-} 
+
+resource "aws_dynamodb_table" "execution-table" {
+  name           = "execution-table"
+  hash_key       = "execution_id"
+  billing_mode   = "PROVISIONED"
+  write_capacity = 10
+  read_capacity  = 10
+  attribute {
+    name = "execution_id"
+    type = "S"
+  }
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+  ttl {
+    enabled        = true
+    attribute_name = "expiryPeriod"
+  }
+  global_secondary_index {
+    name            = "user_id"
+    hash_key        = "user_id"
+    write_capacity  = 10
+    read_capacity   = 10
+    projection_type = "ALL"
+  }
+  point_in_time_recovery { enabled = true }
+  server_side_encryption { enabled = true }
+}
 
 
-resource "aws_s3_bucket_public_access_block" "access_block_executions"{
+resource "aws_s3_bucket_public_access_block" "access_block_executions" {
   bucket = aws_s3_bucket.s3bucket_executions.id
 
   block_public_acls       = true
@@ -71,7 +86,7 @@ resource "aws_s3_bucket_public_access_block" "access_block_executions"{
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-resource "aws_s3_bucket_public_access_block" "access_block_uploads"{
+resource "aws_s3_bucket_public_access_block" "access_block_uploads" {
   bucket = aws_s3_bucket.s3bucket_uploads.id
 
   block_public_acls       = true

b313a1256dc494c7b7978f46d4140a49ba9aba37
Daniel Wu <dwu359@gatech.edu>
2023-01-14 16:50:28 -0500

    mock aws lambda

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,11 +2,56 @@ import pytest
 from backend.aws_helpers.lambda_utils.lambda_client import invoke
 import json
 import pandas as pd
+from moto import mock_iam, mock_lambda
+import boto3
+import zipfile
 
 @pytest.fixture
 def read_csv_to_json():
     df = pd.read_csv("tests/test_data/iris.csv")
     return df.to_dict("records")
+
+@mock_iam    
+@mock_lambda
+def invoke_preprocess_lambda(payload):
+    session = boto3.Session(
+      aws_access_key_id="fake_access_key",
+      aws_secret_access_key="fake_secret_key",
+      region_name="us-west-2"
+    )
+    iam_client = session.client("iam")
+    role = iam_client.create_role(
+        RoleName="preprocess_test_role",
+        AssumeRolePolicyDocument=json.dumps({
+            "Statement": [{
+                "Action": "sts:AssumeRole",
+                "Principal": {"Service": "lambda.amazonaws.com"},
+                "Effect": "Allow"
+            }]
+        })
+    )
+
+    # Attach necessary permissions to the role
+    iam_client.attach_role_policy(
+        RoleName="preprocess_test_role",
+        PolicyArn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+    )
+    
+    # Get the ARN of the new role
+    role_arn = role["Role"]["Arn"]
+    
+    client = session.client('lambda')
+    client.create_function(
+        FunctionName='preprocess_data',
+        Runtime='python3.9',
+        Role=role_arn,
+        Handler='lambda_function.lambda_handler',
+        Code={'ZipFile': open("dlp-terraform/lambda/preprocess_lambda_function.py", "rb").read()}
+    )
+    response = client.invoke(FunctionName="preprocess_data", Payload=payload)
+    resJson = json.loads(response["Payload"].read())
+    print(resJson)
+    return resJson
     
 @pytest.mark.parametrize(
     "code_snippet",
@@ -19,7 +64,7 @@ def test_rename_column(read_csv_to_json, code_snippet):
             "data": read_csv_to_json,
             "code": code_snippet
     })
-    resJson = invoke('preprocess_data', payload)
+    resJson = invoke_preprocess_lambda(payload)
     assert "columns" in resJson and resJson['columns'] == ['sepal.length', 'sepal.width', 'petal.length', 'petal.width', 'dlp'], resJson
 
 @pytest.mark.parametrize(
@@ -33,7 +78,7 @@ def test_drop_column(read_csv_to_json, code_snippet):
             "data": read_csv_to_json,
             "code": code_snippet
     })
-    resJson = invoke('preprocess_data', payload)
+    resJson = invoke_preprocess_lambda(payload)
     assert "columns" in resJson and resJson['columns'] == ['sepal.length', 'sepal.width', 'petal.length', 'petal.width'], resJson
 
 @pytest.mark.parametrize(
@@ -47,5 +92,5 @@ def test_add_two_columns(read_csv_to_json, code_snippet):
             "data": read_csv_to_json,
             "code": code_snippet
     })
-    resJson = invoke('preprocess_data', payload)
+    resJson = invoke_preprocess_lambda(payload)
     assert "columns" in resJson and resJson['columns'] == ['sepal.length', 'sepal.width', 'petal.length', 'petal.width', 'variety', 'test'], resJson

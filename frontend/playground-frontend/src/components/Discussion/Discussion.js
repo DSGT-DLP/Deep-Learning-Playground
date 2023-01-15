d73f34325ebb40615486b301e99b2b49da21bfb7
soshi04 <sirsohumjoshi@gmail.com>
2023-01-14 19:54:43 -0500

    Cleared Giscus code because it's not viable

--- frontend/playground-frontend/src/components/Discussion/Discussion.js
+++ frontend/playground-frontend/src/components/Discussion/Discussion.js
@@ -3,22 +3,7 @@ import Giscus from "@giscus/react";
 
 const Discussion = () => {
   return (
-    <div className="sections" id="discussion">
-      <Giscus
-        repo="noah-iversen/dl-discussions"
-        repoId="R_kgDOIpsdqg"
-        category="General"
-        categoryId="DIC_kwDOIpsdqs4CTLSI"
-        mapping="url"
-        strict="0"
-        reactions-enabled="1"
-        emit-metadata="0"
-        input-position="top"
-        theme="light"
-        lang="en"
-        loading="lazy"
-      />
-    </div>
+    <h1>Container for potential forum solution!</h1>
   );
 };
 export default Discussion;

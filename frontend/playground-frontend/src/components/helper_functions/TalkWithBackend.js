78203bd25fe75473f02997d70a5b40dea19a2530
Faris Durrani <afarisdurrani@gmail.com>
2023-01-21 12:30:29 -0500

    Have modes dev and prod do the same func

--- frontend/playground-frontend/src/components/helper_functions/TalkWithBackend.js
+++ frontend/playground-frontend/src/components/helper_functions/TalkWithBackend.js
@@ -62,6 +62,8 @@ async function train_and_output(choice, choiceDict) {
     return trainResult;
   } else {
     //TODO: submit request to sqs. return success or fail message!
+    const trainResult = await sendToBackend(routeDict[choice], choiceDict);
+    return trainResult;
   }
 }
 

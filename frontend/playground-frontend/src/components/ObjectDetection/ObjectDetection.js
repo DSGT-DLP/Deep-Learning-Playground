605d00b84897ef19d3e6513866a8697661dd2423
Dashrekker <Dashrekker@users.noreply.github.com>
2023-01-23 02:28:25 +0000

    Prettified Code!

--- frontend/playground-frontend/src/components/ObjectDetection/ObjectDetection.js
+++ frontend/playground-frontend/src/components/ObjectDetection/ObjectDetection.js
@@ -22,7 +22,7 @@ import {
 import FilerobotImageEditor, {
   TABS,
   TOOLS,
-} from 'react-filerobot-image-editor';
+} from "react-filerobot-image-editor";
 
 const ObjectDetection = () => {
   const [customModelName, setCustomModelName] = useState(
@@ -78,7 +78,7 @@ const ObjectDetection = () => {
   };
 
   const dataURLtoFile = (dataurl, filename) => {
-    const arr = dataurl.split(',');
+    const arr = dataurl.split(",");
     const mime = arr[0].match(/:(.*?);/)[1];
     const bstr = atob(arr[1]);
     let n = bstr.length;
@@ -121,64 +121,65 @@ const ObjectDetection = () => {
         ))}
       </BackgroundLayout>
       <FilerobotImageEditor
-          source = 'https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg'
-          onSave={(editedImageObject, designState) => {
-            console.log('saved', editedImageObject, designState);
-            const file = dataURLtoFile(editedImageObject.imageBase64, editedImageObject.fullName);
-            setUploadFile(file);
-            
-          }
-          }
-          annotationsCommon={{
-            fill: '#ff0000',
-          }}
-          Text={{ text: 'Filerobot...' }}
-          Rotate={{ angle: 90, componentType: 'slider' }}
-          Crop={{
-            presetsItems: [
-              {
-                titleKey: 'classicTv',
-                descriptionKey: '4:3',
-                ratio: 4 / 3,
-                // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
-              },
-              {
-                titleKey: 'cinemascope',
-                descriptionKey: '21:9',
-                ratio: 21 / 9,
-                // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
-              },
-            ],
-            presetsFolders: [
-              {
-                titleKey: 'socialMedia', // will be translated into Social Media as backend contains this translation key
-                // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
-                groups: [
-                  {
-                    titleKey: 'facebook',
-                    items: [
-                      {
-                        titleKey: 'profile',
-                        width: 180,
-                        height: 180,
-                        descriptionKey: 'fbProfileSize',
-                      },
-                      {
-                        titleKey: 'coverPhoto',
-                        width: 820,
-                        height: 312,
-                        descriptionKey: 'fbCoverPhotoSize',
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          }}
-          tabsIds={[TABS.ADJUST, TABS.ANNOTATE, TABS.WATERMARK]} // or {['Adjust', 'Annotate', 'Watermark']}
-          defaultTabId={TABS.ANNOTATE} // or 'Annotate'
-          defaultToolId={TOOLS.TEXT} // or 'Text'
-        />
+        source="https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg"
+        onSave={(editedImageObject, designState) => {
+          console.log("saved", editedImageObject, designState);
+          const file = dataURLtoFile(
+            editedImageObject.imageBase64,
+            editedImageObject.fullName
+          );
+          setUploadFile(file);
+        }}
+        annotationsCommon={{
+          fill: "#ff0000",
+        }}
+        Text={{ text: "Filerobot..." }}
+        Rotate={{ angle: 90, componentType: "slider" }}
+        Crop={{
+          presetsItems: [
+            {
+              titleKey: "classicTv",
+              descriptionKey: "4:3",
+              ratio: 4 / 3,
+              // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
+            },
+            {
+              titleKey: "cinemascope",
+              descriptionKey: "21:9",
+              ratio: 21 / 9,
+              // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
+            },
+          ],
+          presetsFolders: [
+            {
+              titleKey: "socialMedia", // will be translated into Social Media as backend contains this translation key
+              // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
+              groups: [
+                {
+                  titleKey: "facebook",
+                  items: [
+                    {
+                      titleKey: "profile",
+                      width: 180,
+                      height: 180,
+                      descriptionKey: "fbProfileSize",
+                    },
+                    {
+                      titleKey: "coverPhoto",
+                      width: 820,
+                      height: 312,
+                      descriptionKey: "fbCoverPhotoSize",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        }}
+        tabsIds={[TABS.ADJUST, TABS.ANNOTATE, TABS.WATERMARK]} // or {['Adjust', 'Annotate', 'Watermark']}
+        defaultTabId={TABS.ANNOTATE} // or 'Annotate'
+        defaultToolId={TOOLS.TEXT} // or 'Text'
+      />
       <Spacer height={40} />
       <TitleText text="Image Transformations" />
       <Transforms

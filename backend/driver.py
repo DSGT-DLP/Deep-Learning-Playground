7c39c2ab1c6af3a727aa3e219729df6d9ade8b72
Daniel Wu <dwu359@gatech.edu>
2023-02-23 21:41:42 -0500

    remove print statement

--- backend/driver.py
+++ backend/driver.py
@@ -96,7 +96,6 @@ def tabular_run():
         write_to_bucket(SAVED_MODEL_DL, EXECUTION_BUCKET_NAME, f"{execution_id}/{os.path.basename(SAVED_MODEL_DL)}")
         write_to_bucket(ONNX_MODEL, EXECUTION_BUCKET_NAME, f"{execution_id}/{os.path.basename(ONNX_MODEL)}")
         write_to_bucket(DEEP_LEARNING_RESULT_CSV_PATH, EXECUTION_BUCKET_NAME, f"{execution_id}/{os.path.basename(DEEP_LEARNING_RESULT_CSV_PATH)}")
-        print(execution_id)
         print(train_loss_results)
         updateStatus(execution_id, "SUCCESS")
         return send_train_results(train_loss_results)

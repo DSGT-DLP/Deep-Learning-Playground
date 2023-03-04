6a1706681972e1ab5b1bb4ecbdcb97365f04745d
Thomas Chin <thomas081904@gmail.com>
2023-03-04 09:21:08 -0500

    Fixed create Execution Data

--- backend/aws_helpers/dynamo_db_utils/execution_db.py
+++ backend/aws_helpers/dynamo_db_utils/execution_db.py
@@ -139,7 +139,7 @@ def updateStatus(execution_id: str, status: str, entryData: dict = None) -> str:
         return "{\"status\": \"success\"}"
     except:
         entryData["status"] = status
-        dynamoTable.createUserExecutionsData(entryData)
+        createUserExecutionsData(entryData)
         return "{\"status\": \"success\"}"
 
 

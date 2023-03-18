d512d3c0e98beeb8ca8bb2a820f9f76f19c8c274
Faris Durrani <fdurrani@mythics.com>
2023-03-18 15:04:07 -0400

    reverted setupProxy

---
+++ frontend/playground-frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = (app) => {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://127.0.0.1:8000",
+    })
+  );
+};

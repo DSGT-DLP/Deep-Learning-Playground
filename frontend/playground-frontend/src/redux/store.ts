7f9e5f73a4f012796d3b2b34797a0492680b0571
Daniel Wu <dwu359@gatech.edu>
2023-03-19 21:38:41 -0500

    init

--- frontend/playground-frontend/src/redux/store.ts
+++ frontend/playground-frontend/src/redux/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currentUserReducer from "./userLogin";
 import trainReducer from "./train";
+import { useDispatch } from "react-redux";
 
 const store = configureStore({
   reducer: {
@@ -12,3 +13,5 @@ const store = configureStore({
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;

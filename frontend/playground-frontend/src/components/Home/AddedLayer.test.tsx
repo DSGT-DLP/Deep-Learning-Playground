61dd26b611644e305e0db5ae3e67fcae913319a2
Daniel Wu <dwu359@gatech.edu>
2023-04-03 16:35:17 -0400

    fix test file

--- frontend/playground-frontend/src/components/Home/AddedLayer.test.tsx
+++ frontend/playground-frontend/src/components/Home/AddedLayer.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import AddedLayer from "./AddedLayer";
@@ -7,11 +7,18 @@ describe("AddedLayer_function", () => {
   // Tests that the ondelete function is called when the delete button is clicked and the layer is successfully deleted. tags: [happy path]
   it("test_deletes_layer_successfully", () => {
     const onDeleteMock = jest.fn();
+    const setAddedLayers = jest.fn();
     render(
       <AddedLayer
         thisLayerIndex={0}
-        addedLayers={[{ display_name: "Test Layer", object_name: "My Layer", parameters: {} }]}
-        setAddedLayers={() => {}}
+        addedLayers={[
+          {
+            display_name: "Test Layer",
+            object_name: "My Layer",
+            parameters: {},
+          },
+        ]}
+        setAddedLayers={setAddedLayers}
         onDelete={onDeleteMock}
       />
     );
@@ -22,12 +29,20 @@ describe("AddedLayer_function", () => {
 
   // Tests that the addedlayer component renders a single layer component with its display name and parameters. tags: [happy path]
   it("test_renders_single_layer_component", () => {
+    const onDeleteMock = jest.fn();
+    const setAddedLayers = jest.fn();
     const { getByText } = render(
       <AddedLayer
         thisLayerIndex={0}
-        addedLayers={[{ display_name: "Test Layer", object_name: "My Layer", parameters: {} }]}
-        setAddedLayers={() => {}}
-        onDelete={() => {}}
+        addedLayers={[
+          {
+            display_name: "Test Layer",
+            object_name: "My Layer",
+            parameters: {},
+          },
+        ]}
+        setAddedLayers={setAddedLayers}
+        onDelete={onDeleteMock}
       />
     );
     expect(getByText("Test Layer")).toBeInTheDocument();
@@ -35,15 +50,24 @@ describe("AddedLayer_function", () => {
 
   // Tests that the ondelete function is called when the delete button is clicked and the last layer in the addedlayers array is successfully deleted. tags: [edge case]
   it("test_deletes_last_layer_in_added_layers_array", () => {
+    const setAddedLayers = jest.fn();
     const onDeleteMock = jest.fn();
     render(
       <AddedLayer
         thisLayerIndex={0}
         addedLayers={[
-          { display_name: "Test Layer 1", object_name: "My Layer 1", parameters: {} },
-          { display_name: "Test Layer 2", object_name: "My Layer 2", parameters: {} },
+          {
+            display_name: "Test Layer 1",
+            object_name: "My Layer 1",
+            parameters: {},
+          },
+          {
+            display_name: "Test Layer 2",
+            object_name: "My Layer 2",
+            parameters: {},
+          },
         ]}
-        setAddedLayers={() => {}}
+        setAddedLayers={setAddedLayers}
         onDelete={onDeleteMock}
       />
     );
@@ -54,12 +78,20 @@ describe("AddedLayer_function", () => {
 
   // Tests that the addedlayer component renders a layer with no parameters when the parameters object is empty. tags: [edge case]
   it("test_renders_layer_with_no_parameters", () => {
+    const setAddedLayers = jest.fn();
+    const onDeleteMock = jest.fn();
     render(
       <AddedLayer
         thisLayerIndex={0}
-        addedLayers={[{ display_name: "Test Layer", object_name: "Test Layer 1", parameters: {} }]}
-        setAddedLayers={() => {}}
-        onDelete={() => {}}
+        addedLayers={[
+          {
+            display_name: "Test Layer",
+            object_name: "Test Layer 1",
+            parameters: {},
+          },
+        ]}
+        setAddedLayers={setAddedLayers}
+        onDelete={onDeleteMock}
       />
     );
     expect(screen.queryByRole("textbox")).not.toBeInTheDocument();
@@ -68,6 +100,7 @@ describe("AddedLayer_function", () => {
   // Tests that the addedlayers state is updated when a parameter value is changed. tags: [general behavior]
   it("test_updates_added_layers_state", () => {
     const setAddedLayersMock = jest.fn();
+    const onDeleteMock = jest.fn();
     const { getByRole } = render(
       <AddedLayer
         thisLayerIndex={0}
@@ -75,29 +108,31 @@ describe("AddedLayer_function", () => {
           {
             display_name: "Test Layer",
             object_name: "My Layer",
-            parameters: { param1: 
-                { 
-                    index: 0, 
-                    parameter_name: "My Parameter", 
-                    value: "",
-                    min: undefined,
-                    max: undefined,
-                    parameter_type: "text" 
-                } 
+            parameters: {
+              param1: {
+                index: 0,
+                parameter_name: "My Parameter",
+                value: "",
+                min: null,
+                max: null,
+                parameter_type: "text",
+              },
             },
           },
         ]}
         setAddedLayers={setAddedLayersMock}
-        onDelete={() => {}}
+        onDelete={onDeleteMock}
       />
     );
     const inputBox = getByRole("textbox");
-    userEvent.type(inputBox,  "new value");
+    userEvent.type(inputBox, "new value");
     expect(setAddedLayersMock).toHaveBeenCalled();
   });
 
   // Tests that the parameters object for each layer is converted into an array of parameter objects. tags: [general behavior]
   it("test_converts_parameters_object_to_array", () => {
+    const setAddedLayersMock = jest.fn();
+    const onDeleteMock = jest.fn();
     render(
       <AddedLayer
         thisLayerIndex={0}
@@ -106,25 +141,27 @@ describe("AddedLayer_function", () => {
             display_name: "Test Layer",
             object_name: "My Layer",
             parameters: {
-              param1: { 
-                parameter_name: "Param 1", value: "",
-                min: undefined,
-                max: undefined,
-                parameter_type: "text", 
-                index: 0 
-              },
-              param2: { 
-                parameter_name: "Param 2", value: "",
-                min: undefined,
-                max: undefined,
+              param1: {
+                parameter_name: "Param 1",
+                value: "",
+                min: null,
+                max: null,
                 parameter_type: "text",
-                index: 1  
+                index: 0,
+              },
+              param2: {
+                parameter_name: "Param 2",
+                value: "",
+                min: null,
+                max: null,
+                parameter_type: "text",
+                index: 1,
               },
             },
           },
         ]}
-        setAddedLayers={() => {}}
-        onDelete={() => {}}
+        setAddedLayers={setAddedLayersMock}
+        onDelete={onDeleteMock}
       />
     );
     const paramInputs = screen.getAllByRole("textbox");

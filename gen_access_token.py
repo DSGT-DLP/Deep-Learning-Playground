ee7c0013c5d399804efa980be413671aa74d7edf
karkir0003 <karkir0003@gmail.com>
2023-04-22 11:59:43 -0400

    WIP cont

--- gen_access_token.py
+++ gen_access_token.py
@@ -1,9 +1,20 @@
 import firebase_admin
+import argparse
 from firebase_admin import auth
 
 # initialize the Firebase app
-cred = firebase_admin.credentials.Certificate('/path/to/service_account_key.json')
-firebase_admin.initialize_app(cred)
+config = {
+  'apiKey': "AIzaSyAMJgYSG_TW7CT_krdWaFUBLxU4yRINxX8",
+  'authDomain': "deep-learning-playground-8d2ce.firebaseapp.com",
+  'projectId': "deep-learning-playground-8d2ce",
+  'storageBucket': "deep-learning-playground-8d2ce.appspot.com",
+  'messagingSenderId': "771338023154",
+  'appId': "1:771338023154:web:8ab6e73fc9c646426a606b",
+};
+
+
+cred = firebase_admin.credentials.ApplicationDefault()
+firebase_admin.initialize_app(credential=cred, options=config)
 
 # function to generate a UID for a user based on their email
 def generate_uid(email):
@@ -20,7 +31,14 @@ def generate_token(email):
     token = auth.create_custom_token(uid)
     return token
 
-# example usage
-email = 'example@gmail.com'
-token = generate_token(email)
-print(token)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Script to generate bearer token for backend API testing")
+    parser.add_argument('--email', type=str, help='Email Address to generate bearer token for')
+    args = parser.parse_args()
+    email = args.email
+    return email 
+
+if __name__ == '__main__':
+    email = parse_args()
+    token = generate_token(email)
+    print(f"your token is {token}")

72721e9058561557004a25b05d3fa09111bd6239
Austin Tsang <47958374+Dashrekker@users.noreply.github.com>
2023-05-15 21:52:33 -0400

    added phone number and email address to tabularreviewstep

--- frontend/playground-frontend/src/features/Train/features/Tabular/components/TabularReviewStep.tsx
+++ frontend/playground-frontend/src/features/Train/features/Tabular/components/TabularReviewStep.tsx
@@ -3,6 +3,8 @@ import { TrainspaceData } from "../types/tabularTypes";
 import { Stack, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "@/common/redux/hooks";
 import { updateTabularTrainspaceData } from "../redux/tabularActions";
+import TitleText from "@/common/components/TitleText";
+import { TextField } from "@mui/material";
 
 const TabularReviewStep = ({
   renderStepperButtons,
@@ -13,6 +15,8 @@ const TabularReviewStep = ({
   ) => React.ReactNode;
   setIsModified: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [email, setEmail] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
   const trainspace = useAppSelector(
     (state) => state.trainspace.current as TrainspaceData<"REVIEW"> | undefined
   );
@@ -27,6 +31,14 @@ const TabularReviewStep = ({
       <Typography>{`Is Default?: ${trainspace.datasetData.isDefaultDataset}`}</Typography>
       <Typography>{`Target Column: ${trainspace.parameterData.targetCol}`}</Typography>
       <Typography>{`Feature Columns: ${trainspace.parameterData.features.join()}`}</Typography>
+      <TextField
+        label="Phone Number"
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <TextField
+        label="Email Address"
+        onChange={(e) => setEmail(e.target.value)}
+      />
       {renderStepperButtons((trainspaceData) => {
         dispatch(
           updateTabularTrainspaceData({

eff567c03d7b1c56d66f8786f65bd08a823ade9f
Faris Durrani <fdurrani@mythics.com>
2023-05-18 21:26:31 -0400

    moved to functional dynamo instead of class

--- backend/aws_helpers/dynamo_db_utils/new/dynamo_db_utils.py
+++ backend/aws_helpers/dynamo_db_utils/new/dynamo_db_utils.py
@@ -4,122 +4,113 @@ from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 
-class DynamoDbUtils():
-    def __init__(self):
-        pass
+def get_dynamo_item(table_name: str, keys: dict[str, str or int]) -> dict:
+    """
+    Get item from DynamoDB table that match all keys, raises exception if item does not exist
 
-    def get_item(self, table_name: str, partition_key: str) -> dict:
-        """
-        Get item from DynamoDB table by key, raises exception if item does not exist
-
-        Args:
-            table_name (str): Name of DynamoDB table
-            key (str): Key of item to get
-        
-        Returns:
-            dict: Item from DynamoDB table in JSON format
-
-        Raises:
-            ValueError: If table_name is not a valid table name
-            Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
-        """
-        # Validation steps
-        if table_name not in ALL_DYANMODB_TABLES.keys():
-            raise ValueError("Invalid table name: " + table_name)
-        
-        # Get item
-        item_key = {
-            ALL_DYANMODB_TABLES[table_name]['partition_key']: partition_key
-        }
-        table = dynamodb.Table(table_name)
-        response = table.get_item(Key=item_key)
-        if response.get('Item') is None:
-            raise Exception("Item not found")
-        
-        return response['Item']
+    Args:
+        table_name (str): Name of DynamoDB table
+        key (str): Keys of item to get, e.g., {'id': 'fedfsd', 'name': 'bob'}
     
+    Returns:
+        dict: Item from DynamoDB table in JSON format
 
-    def create_item(self, table_name: str, input_item: dict) -> bool:
-        """
-        Creates item in DynamoDB table, replaces item if partition_key already exists
-
-        Args:
-            table_name (str): Name of DynamoDB table
-            input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
-        
-        Returns:
-            dict: Item from DynamoDB table in JSON format
-
-        Raises:
-            ValueError: If table_name is not a valid table name
-            Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
-        """
-        # Validation steps
-        if table_name not in ALL_DYANMODB_TABLES.keys():
-            raise ValueError("Invalid table name: " + table_name)
-        if input_item.get(ALL_DYANMODB_TABLES[table_name]['partition_key']) is None:
-            raise ValueError("Item must have the partition key")
-
-        item = { k: v for k, v in input_item.items() }
-        # Create item
-        table = dynamodb.Table(table_name)
-        response = table.put_item(Item=item)
-        if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
-            raise Exception("Failed to delete item")
-        return True
+    Raises:
+        ValueError: If table_name is not a valid table name
+        Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
+    """
+    # Validation steps
+    if table_name not in ALL_DYANMODB_TABLES.keys():
+        raise ValueError("Invalid table name: " + table_name)
     
-
-    def update_item(self, table_name: str, input_item: dict) -> bool:
-        """
-        Updates item in DynamoDB table, creates if partition_key does not exist
-
-        Args:
-            table_name (str): Name of DynamoDB table
-            input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
-        
-        Returns:
-            dict: Item from DynamoDB table in JSON format
-
-        Raises:
-            ValueError: If table_name is not a valid table name
-            Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
-        """
-        return self.create_item(table_name, input_item)
+    # Get item
+    table = dynamodb.Table(table_name)
+    response = table.get_item(Key=keys)
+    if response.get('Item') is None:
+        raise Exception("Item not found")
     
+    return response['Item']
 
-    def delete_item(self, table_name: str, partition_key: str) -> bool:
-        """
-        Deletes item from DynamoDB table by key, does nothing if item does not exist
 
-        Args:
-            table_name (str): Name of DynamoDB table
-            key (str): Key of item to delete
+def create_dynamo_item(table_name: str, input_item: dict) -> bool:
+    """
+    Creates item in DynamoDB table, replaces item if partition_key already exists
 
-        Returns:
-            true if the item was created successfully
+    Args:
+        table_name (str): Name of DynamoDB table
+        input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
+    
+    Returns:
+        dict: Item from DynamoDB table in JSON format
 
-        Raises:
-            ValueError: If table_name is not a valid table name
-            Exception: If DynamoDB delete_item call fails for any reason
-        """
-        # Validation steps
-        if table_name not in ALL_DYANMODB_TABLES.keys():
-            raise ValueError("Invalid table name: " + table_name)
-        
-        # Delete item
-        item_key = {
-            ALL_DYANMODB_TABLES[table_name]['partition_key']: partition_key
-        }
-        table = dynamodb.Table(table_name)
-        response = table.delete_item(Key=item_key)
-        if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
-            raise Exception("Failed to delete item")
-        return True
+    Raises:
+        ValueError: If table_name is not a valid table name
+        Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
+    """
+    # Validation steps
+    if table_name not in ALL_DYANMODB_TABLES.keys():
+        raise ValueError("Invalid table name: " + table_name)
+    if input_item.get(ALL_DYANMODB_TABLES[table_name]['partition_key']) is None:
+        raise ValueError("Item must have the partition key")
+
+    item = { k: v for k, v in input_item.items() }
+    # Create item
+    table = dynamodb.Table(table_name)
+    response = table.put_item(Item=item)
+    if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
+        raise Exception("Failed to delete item")
+    return True
+
+
+def update_dynamo_item(table_name: str, input_item: dict) -> bool:
+    """
+    Updates item in DynamoDB table, creates if partition_key does not exist
+
+    Args:
+        table_name (str): Name of DynamoDB table
+        input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
+    
+    Returns:
+        dict: Item from DynamoDB table in JSON format
+
+    Raises:
+        ValueError: If table_name is not a valid table name
+        Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
+    """
+    return create_dynamo_item(table_name, input_item)
+
+
+def delete_dynamo_item(table_name: str, keys: dict[str, str or int]) -> bool:
+    """
+    Deletes item from DynamoDB table by matching given keys, does nothing if item does not exist
+
+    Args:
+        table_name (str): Name of DynamoDB table
+        key (str): Key of item to delete
+
+    Returns:
+        true if the item was created successfully
+
+    Raises:
+        ValueError: If table_name is not a valid table name
+        Exception: If DynamoDB delete_item call fails for any reason
+    """
+    # Validation steps
+    if table_name not in ALL_DYANMODB_TABLES.keys():
+        raise ValueError("Invalid table name: " + table_name)
+    
+    # Delete item
+    table = dynamodb.Table(table_name)
+    response = table.delete_item(Key=keys)
+    if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
+        raise Exception("Failed to delete item")
+    return True
 
 
 
 if __name__ == "__main__":
     print(1)
-    # print(DynamoDbUtils().update_item("trainspace", "bla"))
-    print(DynamoDbUtils().update_item("trainspace", 
+    # print(delete_dynamo_item("trainspace", {"trainspace_id": "blah"}))
+    # exit()
+    print(update_dynamo_item("trainspace", 
                                       {"trainspace_id": "blah", "uid": "bleh", "created": datetime.now().isoformat()}))

8a9026f81e8ee4b59501bc639ea9d90d0fd44b93
Faris Durrani <fdurrani@mythics.com>
2023-05-18 22:10:51 -0400

    allowed individual update to status

--- backend/aws_helpers/dynamo_db_utils/new/dynamo_db_utils.py
+++ backend/aws_helpers/dynamo_db_utils/new/dynamo_db_utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 
-def get_dynamo_item(table_name: str, keys: dict[str, str or int]) -> dict:
+def get_dynamo_item(table_name: str, keys: dict) -> dict:
     """
     Get item from DynamoDB table that match all keys, raises exception if item does not exist
 
@@ -38,37 +38,43 @@ def create_dynamo_item(table_name: str, input_item: dict) -> bool:
 
     Args:
         table_name (str): Name of DynamoDB table
-        input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
+        input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}.
     
     Returns:
         dict: Item from DynamoDB table in JSON format
 
     Raises:
-        ValueError: If table_name is not a valid table name
+        ValueError: If table_name is not a valid table name, or the input dict is missing the partition key or gsi key (if it exists)
         Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
     """
     # Validation steps
     if table_name not in ALL_DYANMODB_TABLES.keys():
         raise ValueError("Invalid table name: " + table_name)
-    if input_item.get(ALL_DYANMODB_TABLES[table_name]['partition_key']) is None:
-        raise ValueError("Item must have the partition key")
+    partition_key = ALL_DYANMODB_TABLES[table_name]['partition_key']
+    if input_item.get(partition_key) is None:
+        raise ValueError("Item must have the partition key: " + partition_key)
+    gsi_key = ALL_DYANMODB_TABLES[table_name].get('gsi')
+    if gsi_key and input_item.get(gsi_key) is None:
+        raise ValueError("Item must have the gsi key: " + gsi_key)
 
-    item = { k: v for k, v in input_item.items() }
     # Create item
     table = dynamodb.Table(table_name)
-    response = table.put_item(Item=item)
+    response = table.put_item(Item=input_item)
     if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
         raise Exception("Failed to delete item")
     return True
 
 
-def update_dynamo_item(table_name: str, input_item: dict) -> bool:
+def update_dynamo_item(
+        table_name: str, 
+        keys: dict[str, str or int], 
+        update_items: dict) -> bool:
     """
     Updates item in DynamoDB table, creates if partition_key does not exist
 
     Args:
         table_name (str): Name of DynamoDB table
-        input_item (dict): object to insert into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}. Values can only be strings or numbers
+        update_items (dict): object containing the columnds and values to update into table in the form of a dictionary e.g. {'id': 'fesd', 'name': 'bob'}
     
     Returns:
         dict: Item from DynamoDB table in JSON format
@@ -77,10 +83,27 @@ def update_dynamo_item(table_name: str, input_item: dict) -> bool:
         ValueError: If table_name is not a valid table name
         Exception: If DynamoDB get_item call fails for any reason, e.g., item not found
     """
-    return create_dynamo_item(table_name, input_item)
+    # create update expression
+    update_expression = "SET " + ', '.join([f"{key} = :{key}" for key in update_items.keys()])
+
+    # create expression attribute values
+    expression_attribute_values = {f":{key}": value for key, value in update_items.items()}
+
+    print(keys, update_expression, expression_attribute_values)
+
+    # Create item
+    table = dynamodb.Table(table_name)
+    response = table.update_item(
+        Key=keys, 
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=expression_attribute_values
+    )
+    if (response['ResponseMetadata']['HTTPStatusCode'] != 200):
+        raise Exception("Failed to delete item")
+    return True
 
 
-def delete_dynamo_item(table_name: str, keys: dict[str, str or int]) -> bool:
+def delete_dynamo_item(table_name: str, keys: dict) -> bool:
     """
     Deletes item from DynamoDB table by matching given keys, does nothing if item does not exist
 
@@ -107,10 +130,10 @@ def delete_dynamo_item(table_name: str, keys: dict[str, str or int]) -> bool:
     return True
 
 
-
-if __name__ == "__main__":
-    print(1)
-    # print(delete_dynamo_item("trainspace", {"trainspace_id": "blah"}))
-    # exit()
-    print(update_dynamo_item("trainspace", 
-                                      {"trainspace_id": "blah", "created": datetime.now().isoformat()}))
+# if __name__ == "__main__":
+#     print(1)
+#     # print(getTrainspaceData("blah"))
+#     # exit()
+#     print(update_dynamo_item('trainspace', {'trainspace_id': 'blah'},
+#                                       {
+#                                           "created": datetime.now().isoformat()}))
--- backend/aws_helpers/dynamo_db_utils/new/trainspace_db.py
+++ backend/aws_helpers/dynamo_db_utils/new/trainspace_db.py
@@ -1,8 +1,9 @@
 from backend.aws_helpers.dynamo_db_utils.new.dynamo_db_utils import get_dynamo_item, create_dynamo_item, update_dynamo_item, delete_dynamo_item
+from datetime import datetime
 
 TABLE_NAME = 'trainspace'
 
-def getTrainspaceData(trainspace_id: str) -> dict:
+def getTrainspaceData(trainspace_id: str) -> dict[str, str or int]:
     """
     Retrieves an entry from the `execution-table` DynamoDB table given an `execution_id`. Example output: {"execution_id": "blah", "user_id": "blah", "name": "blah", "timestamp": "blah", "data_source": "TABULAR", "status": "QUEUED", "progress": 1}
 
@@ -13,19 +14,44 @@ def getTrainspaceData(trainspace_id: str) -> dict:
     return record
 
 
-def updateTrainspaceData(requestData: dict) -> bool:
+def updateTrainspaceData(trainspace_id: str, requestData: dict) -> bool:
     """
     Updates an entry from the `execution-table` DynamoDB table given an `execution_id`.
     @param requestData: A dictionary containing the execution_id and other table attributes to be updated, with user_id as a required field
     @return a success status message if the update is successful
     """
-    if not validate_keys(requestData, REQUIRED_KEYS):
-        raise ValueError(f"Missing keys {REQUIRED_KEYS} in request body")
+    return update_dynamo_item(TABLE_NAME, {'trainspace_id': trainspace_id},requestData)
 
-    dynamoTable = TrainspaceDDBUtil(TRAINSPACE_TABLE_NAME, AWS_REGION)
-    trainspace_id = requestData[PRIMARY_KEY]
-    updatedRecord = TrainspaceData(**requestData).__dict__
-    updatedRecord.pop(PRIMARY_KEY)
-    dynamoTable.update_record(trainspace_id, **updatedRecord)
-    return '{"status": "success"}'
 
+def getAllUserTrainspaceData(user_id: str) -> dict[str, str or int]:
+    """
+    Retrieves an entry from the `execution-table` DynamoDB table given an `execution_id`. Example output: {"execution_id": "blah", "user_id": "blah", "name": "blah", "timestamp": "blah", "data_source": "TABULAR", "status": "QUEUED", "progress": 1}
+
+    @param execution_id: The execution_id of the entry to be retrieved
+    @return: A JSON string of the entry retrieved from the table
+    """
+    response = get_dynamo_item(TABLE_NAME, {'uid': user_id})
+    return response
+
+def updateStatus(trainspace_id: str, status: str, entryData: dict = None) -> str:
+    """
+    Updates the status of a trainspace entry in the `trainspace` DynamoDB table given a `trainspace_id`. Also updates the entry with the given entryData if provided.
+    @param trainspace_id: The trainspace_id of the entry to be updated
+    @param status: The status to be updated
+    @param entryData: The entry to be updated
+    @return a success status message if the update is successful
+    """
+    if entryData is None:
+        entryData = {}
+    entryData['status'] = status
+    return updateTrainspaceData(trainspace_id, entryData)
+
+
+if __name__ == "__main__":
+    print(1)
+    # print(getTrainspaceData("blah"))
+    # exit()
+    print(updateTrainspaceData(
+                                      {
+                                          "trainspace_id": "blah",
+                                          "created": datetime.now().isoformat()}))

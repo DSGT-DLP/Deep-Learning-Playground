1a2965ff51b0782e2535ba01e969238681f714a6
noahiversen <noahiversen24@gmail.com>
2023-05-26 12:14:31 -0400

    Tmux script typo

--- tmux-script.sh
+++ tmux-script.sh
@@ -4,5 +4,5 @@ tmux new-session -d -s $session
 tmux rename-window -t 0 'frontend'
 tmux send-keys -t 'frontend' 'yarn run startf' C-m
 tmux new-window -t $session:1 -n 'backend'
-tmux send-keys -t $session:1 'conda activate dlplayground && pythom -m backend.driver' C-m
+tmux send-keys -t $session:1 'conda activate dlplayground && python -m backend.driver' C-m
 tmux attach-session -t $session:0

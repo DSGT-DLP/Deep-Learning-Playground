a69c5518033a29d366853c6db3704557fe0f4ea4
karkir0003 <karkir0003@gmail.com>
2023-05-26 18:36:51 -0400

    updated test_imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,13 @@
 def test_all_imports():
-    import backend.aws_helpers.dynamo_db_utils.base_db
+    import backend.aws_helpers.aws_rekognition_utils.rekognition_client
+    import backend.aws_helpers.aws_secrets_utils.build_env
     import backend.aws_helpers.dynamo_db_utils.execution_db
+    import backend.aws_helpers.dynamo_db_utils.file_upload_db
+    import backend.aws_helpers.dynamo_db_utils.trainspace_db
+    import backend.aws_helpers.dynamo_db_utils.userprogress_db
+    import backend.aws_helpers.lambda_utils.lambda_client
+    import backend.aws_helpers.s3_utils.s3_client
+    import backend.aws_helpers.sqs_utils.sqs_client
     import backend.common.constants
     import backend.common.dataset
     import backend.common.default_datasets

83a55966783328c82f4a47fe13e0de3deeb5d272
Faris Durrani <fdurrani@mythics.com>
2023-05-30 20:08:05 -0400

    removed calls to playground-frontend

--- backend/aws_helpers/aws_secrets_utils/aws_constants.py
+++ backend/aws_helpers/aws_secrets_utils/aws_constants.py
@@ -8,8 +8,6 @@ ENV_KEYS = [
     "REACT_APP_FEEDBACK_EMAIL",
 ]
 
-FINAL_ENV_PATH = os.path.abspath(
-    os.path.join(os.getcwd(), "frontend", "playground-frontend", ".env")
-)
+FINAL_ENV_PATH = os.path.abspath(os.path.join(os.getcwd(), "frontend", ".env"))
 
 SECRET_NAME = "frontend_env"

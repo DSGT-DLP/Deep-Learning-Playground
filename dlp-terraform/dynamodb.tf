16a11242a1f5ad08d055dc3645077724b6ea9330
AmyCGT <104514492+AmyCGT@users.noreply.github.com>
2023-06-07 22:05:19 -0400

    terraform file cleanup #785
    separated dlp.tf into dynamodb.tf and S3.tf

--- dlp-terraform/dynamodb.tf
+++ dlp-terraform/dynamodb.tf
@@ -13,41 +13,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "s3bucket_executions" {
-  bucket = "dlp-executions-bucket"
-
-  tags = {
-    Name        = "Execution data"
-    Environment = "Dev"
-  }
-}
-resource "aws_s3_bucket" "s3bucket_uploads" {
-  bucket = "dlp-upload-bucket"
-
-  tags = {
-    Name        = "Upload data"
-    Environment = "Dev"
-  }
-}
-resource "aws_s3_bucket_versioning" "upload_versioning" {
-  bucket = aws_s3_bucket.s3bucket_uploads.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "upload_bucket_config" {
-  bucket = aws_s3_bucket.s3bucket_uploads.bucket
-  rule {
-    expiration {
-      days = 5
-    }
-    id     = "logs"
-    status = "Enabled"
-  }
-}
-
 resource "aws_dynamodb_table" "execution-table" {
   name           = "execution-table"
   hash_key       = "execution_id"
@@ -198,34 +163,5 @@ resource "aws_appautoscaling_policy" "dynamodb_table_userprogress_write_policy" 
     }
 
     target_value = 70
-  }
-}
-
-
-resource "aws_s3_bucket_public_access_block" "access_block_executions" {
-  bucket = aws_s3_bucket.s3bucket_executions.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-resource "aws_s3_bucket_public_access_block" "access_block_uploads" {
-  bucket = aws_s3_bucket.s3bucket_uploads.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-resource "aws_s3_bucket_cors_configuration" "uploads_cors_configuration" {
-  bucket = aws_s3_bucket.s3bucket_uploads.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "PUT", "POST", "HEAD", "DELETE"]
-    allowed_origins = ["*"]
-    expose_headers  = []
-    max_age_seconds = 3000
   }
 }

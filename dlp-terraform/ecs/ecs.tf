7e84ecb49d08cee0896151a9ccd86d5867ea2a9a
noahiversen <noahiversen24@gmail.com>
2023-06-13 19:12:13 -0400

    Deploy EC2 instance with SSH key (test commit)

--- dlp-terraform/ecs/ecs.tf
+++ dlp-terraform/ecs/ecs.tf
@@ -38,6 +38,52 @@ resource "aws_ecs_service" "dlp-training-service" {
     assign_public_ip = true
   }
 }
+
+resource "aws_instance" "dlp-ec2-instance" {
+  ami                    = "ami-07dfed28fcf95241c"
+  instance_type          = "t2.micro"
+  key_name               = "aws_key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  tags = {
+    Name = "dlp-ec2-instance-test"
+  }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFPoGs5b17w2yuwZLUbIvj/nJvaPmqOxTxtlpqaWGwFtnO8uKPeyFAHPwLZn31jqJmgBFyCS/6wqetIZLZE7b9I+U9s8FyxvKs+kPqTvvTi47etN7mKLMiTBm8m+mSH5Knljli/C0bdN9CCWKRnoa4hU++hJspWOBKgvmY3TpT8UBJt2Ht9IDy1yzaoEGJ6c31aV4dehlzFcUCx4cV/b6AtigJCisBoXIrV4HF7XuJSYUnXPqbXD7s7lvCRFc6p1z/S2+E7fR4V76mQ1HbusU/gcU3sFZ/qs+tfu2mxx8YBz09vACO+d8eBBsRLSESGbhjKMDQD6lYzjTfa9uHYkbx noahi24@Noah-ASUS"
+}
+
 resource "aws_appautoscaling_target" "dev_to_target" {
   max_capacity       = 1
   min_capacity       = 1
@@ -81,7 +127,7 @@ resource "aws_appautoscaling_policy" "dlp-queue-size-too-small-policy" {
 
     step_adjustment {
 
-      metric_interval_upper_bound = 0 
+      metric_interval_upper_bound = 0
       scaling_adjustment          = 1
     }
   }

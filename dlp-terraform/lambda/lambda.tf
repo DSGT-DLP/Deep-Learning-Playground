de67166227602879c1c2b80f31fa87e40f2e5b7c
Faris Durrani <afarisdurrani@gmail.com>
2023-06-13 21:49:54 -0400

    Tf validate action (#816)
    * test tf validate
    
    * test tf validate
    
    * multiple working directory for tf validate
    
    * lamda tf fix
    
    * added paths for tf validate
    
    * removed python check for dlp-terraform

--- dlp-terraform/lambda/lambda.tf
+++ dlp-terraform/lambda/lambda.tf
@@ -34,37 +34,37 @@ resource "aws_lambda_function_url" "send_email_url" {
 }
 
 resource "aws_apigatewayv2_api" "send_email" {
-  name = "send_email"
+  name          = "send_email"
   protocol_type = "HTTP"
-  
+
 }
 resource "aws_apigatewayv2_stage" "default" {
-  api_id = aws_apigatewayv2_api.send_email.id
-  name   = "default"
+  api_id      = aws_apigatewayv2_api.send_email.id
+  name        = "default"
   auto_deploy = true
 }
 
 resource "aws_apigatewayv2_integration" "send_email_integration" {
-  api_id = aws_apigatewayv2_api.send_email.id
-  integration_uri = aws_lambda_function.send_email.invoke_arn
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.send_email.id
+  integration_uri        = aws_lambda_function.send_email.invoke_arn
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "set_send_email" {
   api_id = aws_apigatewayv2_api.send_email.id
-  
+
   route_key = "POST /send_email"
-  target = "integrations/${aws_apigatewayv2_integration.send_email_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.send_email_integration.id}"
 }
 
 resource "aws_lambda_permission" "api_gw_send_email" {
-  statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.send_email.function_name
-  principal = "apigateway.amazonaws.com"
-  
+  principal     = "apigateway.amazonaws.com"
+
   source_arn = "${aws_apigatewayv2_api.send_email.execution_arn}/*/*/send_email"
 }
 data "archive_file" "preprocess_data_files" {
@@ -78,10 +78,10 @@ resource "aws_lambda_function" "preprocess_data" {
   function_name = "preprocess_data"
   role          = "arn:aws:iam::521654603461:role/service-role/preprocess_data-role-9328ks4z"
   handler       = "preprocess_lambda_function.lambda_handler"
-  layers = ["arn:aws:lambda:us-west-2:336392948345:layer:AWSSDKPandas-Python39:2", "arn:aws:lambda:us-west-2:770693421928:layer:Klayers-p39-numpy:9"]
+  layers        = ["arn:aws:lambda:us-west-2:336392948345:layer:AWSSDKPandas-Python39:2", "arn:aws:lambda:us-west-2:770693421928:layer:Klayers-p39-numpy:9"]
 
   source_code_hash = filebase64sha256("outputs/preprocess_data.zip")
-  runtime = "python3.9"
+  runtime          = "python3.9"
 }
 
 resource "aws_lambda_function_url" "preprocess_data_url" {

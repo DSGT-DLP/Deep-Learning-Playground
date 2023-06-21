e2fc9dd3eeca05f840cde90da8a7e0f4786329b9
Faris Durrani <fdurrani@mythics.com>
2023-06-20 21:46:36 -0400

    added check on api key

--- frontend/src/features/OpenAi/openAiUtils.ts
+++ frontend/src/features/OpenAi/openAiUtils.ts
@@ -5,6 +5,8 @@ const OPEN_AI_API_KEY = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY_DLP002;
 export async function askChatGpt(
   question: string
 ): Promise<string | undefined> {
+  if (!OPEN_AI_API_KEY) throw new Error("OPEN_AI_API_KEY is missing");
+
   const configuration = new Configuration({
     apiKey: OPEN_AI_API_KEY,
   });

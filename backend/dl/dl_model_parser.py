90e8480f3d70851cc836b9871b8f39058469e072
Faris Durrani <fdurrani@mythics.com>
2023-07-06 21:20:16 -0400

    parse deep user arch fix

--- backend/dl/dl_model_parser.py
+++ backend/dl/dl_model_parser.py
@@ -9,45 +9,33 @@ from torch.autograd import Variable
 import torchvision.transforms as transforms
 
 
-def parse_deep_user_architecture(user_model):
+def parse_deep_user_architecture(layers):
     """
     Given a list of user_models in a certain order,
     each layer should contain name and the values of each
     parameter
 
-    Eg: ["Linear(in_features=50, out_features=10)"] should become [nn.Linear(in_features=50, out_features=10)]
-
-    ["nn.Linear(in_features=50, out_features=10)", "nn.Conv2d(in_channels=16, out_channels=33, kernel_size=3, stride=2)"] should become
-    [nn.Linear(in_features=50, out_features=10), nn.Conv2d(in_channels=16, out_channels=33, kernel_size=3, stride=2)]
+    Eg: layers = [{ 'value': 'LINEAR', 'parameters': [10, 3] }] should become [nn.Linear(10, 3)]
 
     Args:
-        user_model (list): string form where you have the model name and the values for each parameter
+        layers (list): string form where you have the model name and the values for each parameter
     Returns:
         parsed_modules (list): list of nn.Module objects parsed
     """
 
     # JSON file will contain name of model along with what hyperparameters need to be specified
-    parsed_modules = []
-    for element in user_model:
-        layer = get_object(element)
-        parsed_modules.append(layer)
-    return parsed_modules
+    converted_data = []
 
+    for item in layers:
+        value = item['value']
+        parameters = item['parameters']
+        
+        if value == 'LINEAR':
+            linear_layer = nn.Linear(*parameters)
+            converted_data.append(linear_layer)
 
-def get_object(element):
-    """
-    Given a string representation of a model layer or object, return the properly
-    instantiated object (nn.Module instance in most scenarios for deep learning)
+    return converted_data
 
-    Args:
-        element (string): string representation of torch module
-
-    return:
-        Instantiated object behind the "string representation of the instance"
-    """
-    return eval(
-        element.replace("'", "")
-    )  # takes in the string representation and returns the "instantiated object"
 
 
 if __name__ == "__main__":

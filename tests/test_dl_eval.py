e6e958457470f104199fed374cdb28e9e2e5dff7
yeaitsurya <surya.subramanian@warriorlife.net>
2023-07-07 10:13:34 -0700

    Added additional unit tests for edge cases
    Added 0 accuracy and between 0 and 1 Accuracy Unit tests

--- tests/test_dl_eval.py
+++ tests/test_dl_eval.py
@@ -24,3 +24,15 @@ def test_compute_accuracy():
 
     # Check if the accuracy is correct
     assert accuracy == 1
+    
+     # Test case: Accuracy of 0
+    predicted_none_correct = torch.tensor([[0.9, 0.1, 0.0], [0.7, 0.2, 0.1]])
+    actual_none_correct = torch.tensor([2, 1])
+    accuracy_none_correct = compute_accuracy(predicted_none_correct, actual_none_correct)
+    assert accuracy_none_correct == 0
+
+    # Test case: Accuracy between 0 and 1
+    predicted_mixed = torch.tensor([[0.8, 0.1, 0.1], [0.2, 0.4, 0.4]])
+    actual_mixed = torch.tensor([0,0])
+    accuracy_mixed = compute_accuracy(predicted_mixed, actual_mixed)
+    assert accuracy_mixed == 0.5

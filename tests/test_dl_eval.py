df245717a7c391db12b0b0a6ca4d27f6a72a2708
yeaitsurya <surya.subramanian@warriorlife.net>
2023-07-07 17:46:13 -0700

    added Pytest parameterized tests
    added Pytest parameterized testing

--- tests/test_dl_eval.py
+++ tests/test_dl_eval.py
@@ -4,37 +4,33 @@ import torch.nn as nn
 from backend.dl.dl_eval import compute_correct, compute_accuracy
 
 
-def test_compute_correct():
-    # Create dummy tensors
-    predicted = torch.tensor([[0.1, 0.2, 0.7], [0.3, 0.4, 0.3]])
-    actual = torch.tensor([2, 1])
-
+@pytest.mark.parametrize("predicted, actual, expected_correct", [
+    # Test case: All correct predictions
+    (torch.tensor([[0.1, 0.2, 0.7], [0.3, 0.4, 0.3]]), torch.tensor([2, 1]), 2),
+     # Test case: Some correct predictions
+    (torch.tensor([[0.8, 0.1, 0.1], [0.2, 0.4, 0.4]]), torch.tensor([0, 0]), 1),
+    # Test case: No correct predictions
+    (torch.tensor([[0.9, 0.1, 0.0], [0.7, 0.2, 0.1]]), torch.tensor([2, 1]), 0),
+])
+def test_compute_correct(predicted, actual, expected_correct):
     # Compute the number of correct predictions
     correct = compute_correct(predicted, actual)
 
     # Check if the number of correct predictions is correct
-    assert correct == 2
+    assert correct == expected_correct
 
 
-def test_compute_accuracy():
-    # Create dummy tensors
-    predicted = torch.tensor([[0.1, 0.2, 0.7], [0.3, 0.4, 0.3]])
-    actual = torch.tensor([2, 1])
-
+@pytest.mark.parametrize("predicted, actual, expected_accuracy", [
+    # Test case: Accuracy of 1
+    (torch.tensor([[0.1, 0.2, 0.7], [0.3, 0.4, 0.3]]), torch.tensor([2, 1]), 1),
+    # Test case: Accuracy between 0 and 1
+    (torch.tensor([[0.8, 0.1, 0.1], [0.2, 0.4, 0.4]]), torch.tensor([0, 0]), 0.5),
+    # Test case: Accuracy of 0
+    (torch.tensor([[0.9, 0.1, 0.0], [0.7, 0.2, 0.1]]), torch.tensor([2, 1]), 0),
+])
+def test_compute_accuracy(predicted, actual, expected_accuracy):
     # Compute the accuracy
     accuracy = compute_accuracy(predicted, actual)
 
     # Check if the accuracy is correct
-    assert accuracy == 1
-    
-    # Test case: Accuracy of 0
-    predicted_none_correct = torch.tensor([[0.9, 0.1, 0.0], [0.7, 0.2, 0.1]])
-    actual_none_correct = torch.tensor([2, 1])
-    accuracy_none_correct = compute_accuracy(predicted_none_correct, actual_none_correct)
-    assert accuracy_none_correct == 0
-
-    # Test case: Accuracy between 0 and 1
-    predicted_mixed = torch.tensor([[0.8, 0.1, 0.1], [0.2, 0.4, 0.4]])
-    actual_mixed = torch.tensor([0,0])
-    accuracy_mixed = compute_accuracy(predicted_mixed, actual_mixed)
-    assert accuracy_mixed == 0.5
+    assert accuracy == expected_accuracy

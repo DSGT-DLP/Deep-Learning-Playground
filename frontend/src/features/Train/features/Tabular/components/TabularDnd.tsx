08104fd34f34b95ee9eaa78ea65167f6321fad8e
karkir0003 <karkir0003@gmail.com>
2023-07-08 21:47:52 -0400

    tabular dnd

--- frontend/src/features/Train/features/Tabular/components/TabularDnd.tsx
+++ frontend/src/features/Train/features/Tabular/components/TabularDnd.tsx
@@ -16,7 +16,7 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-export default function App() {
+export default function TabularDnd() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 

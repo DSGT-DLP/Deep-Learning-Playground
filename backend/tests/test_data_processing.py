ba2708cec635b3e48aebbac2a63483b6aa9baab9
codingwithsurya <codingwithsurya@users.noreply.github.com>
2023-08-02 00:25:33 +0000

    :art: Format Python code with psf/black

--- backend/tests/test_data_processing.py
+++ backend/tests/test_data_processing.py
@@ -11,6 +11,7 @@ from common.dataset import read_dataset
 # Define the S3 bucket name
 S3_BUCKET_NAME = "dlp-upload-bucket"
 
+
 @pytest.mark.parametrize(
     "url,path_to_file",
     [
@@ -29,13 +30,13 @@ def test_dataset_url_reading(url, path_to_file):
         read_dataset(url)
 
         # Upload the test data to the S3 bucket before running the test
-        with open(path_to_file, 'rb') as f:
-            s3 = boto3.client('s3')
+        with open(path_to_file, "rb") as f:
+            s3 = boto3.client("s3")
             s3.put_object(Bucket=S3_BUCKET_NAME, Key="data.csv", Body=f)
 
         # Download the CSV data from S3
         s3_response = s3.get_object(Bucket=S3_BUCKET_NAME, Key="data.csv")
-        s3_data = s3_response['Body'].read().decode("utf8")
+        s3_data = s3_response["Body"].read().decode("utf8")
 
         with open(path_to_file) as f:
             expected_data = f.read()

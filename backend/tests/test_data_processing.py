20755da75dad1b22c287bfc42c706f8aa074fe53
yeaitsurya <surya.subramanian@warriorlife.net>
2023-08-02 16:30:04 -0700

    added a fixture s3_client that creates the mock S3 client

--- backend/tests/test_data_processing.py
+++ backend/tests/test_data_processing.py
@@ -9,8 +9,14 @@ from common.dataset import read_dataset
 
 # Define the S3 bucket name
 S3_BUCKET_NAME = "dlp-upload-bucket"
-S3_REGION = "us-west-2"
+S3_REGION = "us-west-2" 
 
+@pytest.fixture
+def s3_client():
+    with mock_s3():
+        s3 = boto3.client("s3", region_name=S3_REGION)
+        s3.create_bucket(Bucket=S3_BUCKET_NAME, CreateBucketConfiguration={"LocationConstraint": S3_REGION})
+        yield s3
 
 @pytest.mark.parametrize(
     "url,path_to_file",
@@ -25,18 +31,13 @@ S3_REGION = "us-west-2"
         ),
     ],
 )
-@mock_s3
-def test_dataset_url_reading(url, path_to_file):
+def test_dataset_url_reading(url, path_to_file, s3_client):
     try:
-        # Create the S3 bucket before running the test
-        s3 = boto3.client("s3", region_name=S3_REGION)
-        s3.create_bucket(Bucket=S3_BUCKET_NAME)
-
         # Call read_dataset, which should upload the CSV to the mock S3 bucket
         read_dataset(url)
 
         # Download the CSV data from the mock S3 bucket
-        s3_response = s3.get_object(Bucket=S3_BUCKET_NAME, Key="data.csv")
+        s3_response = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key="data.csv")
         s3_data = s3_response["Body"].read().decode("utf-8")
 
         with open(path_to_file) as f:
@@ -48,7 +49,6 @@ def test_dataset_url_reading(url, path_to_file):
     except Exception as e:
         pytest.fail(str(e))
 
-
 @pytest.mark.parametrize(
     "url,path_to_file",
     [
@@ -56,7 +56,6 @@ def test_dataset_url_reading(url, path_to_file):
         ("csv.com/cars.csv", "./tests/expected/cars.csv"),
     ],
 )
-@mock_s3
-def test_dataset_invalid_url(url, path_to_file):
+def test_dataset_invalid_url(url, path_to_file, s3_client):
     with pytest.raises(Exception):
         read_dataset(url)

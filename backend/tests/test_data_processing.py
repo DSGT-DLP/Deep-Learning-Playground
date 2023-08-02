4ed0691a70257318b100e53bb785bc08ee894581
yeaitsurya <surya.subramanian@warriorlife.net>
2023-08-02 16:18:33 -0700

    editing unit tests for integration with moto

--- backend/tests/test_data_processing.py
+++ backend/tests/test_data_processing.py
@@ -1,16 +1,14 @@
-# Import necessary libraries
-from urllib.error import URLError
 import pytest
 import csv
 import boto3
 from botocore.exceptions import ClientError
 from io import StringIO
+from moto import mock_s3
 from common.constants import *
 from common.dataset import read_dataset
 
 # Define the S3 bucket name
 S3_BUCKET_NAME = "dlp-upload-bucket"
-
 
 @pytest.mark.parametrize(
     "url,path_to_file",
@@ -25,28 +23,28 @@ S3_BUCKET_NAME = "dlp-upload-bucket"
         ),
     ],
 )
+@mock_s3
 def test_dataset_url_reading(url, path_to_file):
     try:
+        # Create the S3 bucket before running the test
+        s3 = boto3.client("s3")
+        s3.create_bucket(Bucket=S3_BUCKET_NAME)
+
+        # Call read_dataset, which should upload the CSV to the mock S3 bucket
         read_dataset(url)
 
-        # Upload the test data to the S3 bucket before running the test
-        with open(path_to_file, "rb") as f:
-            s3 = boto3.client("s3")
-            s3.put_object(Bucket=S3_BUCKET_NAME, Key="data.csv", Body=f)
-
-        # Download the CSV data from S3
+        # Download the CSV data from the mock S3 bucket
         s3_response = s3.get_object(Bucket=S3_BUCKET_NAME, Key="data.csv")
-        s3_data = s3_response["Body"].read().decode("utf8")
+        s3_data = s3_response["Body"].read().decode("utf-8")
 
         with open(path_to_file) as f:
             expected_data = f.read()
 
-        # Compare the content of the CSV file uploaded to S3 with the expected CSV file
+        # Compare the content of the CSV file uploaded to the mock S3 bucket with the expected CSV file
         assert s3_data == expected_data
 
     except Exception as e:
         pytest.fail(str(e))
-
 
 @pytest.mark.parametrize(
     "url,path_to_file",
@@ -55,6 +53,7 @@ def test_dataset_url_reading(url, path_to_file):
         ("csv.com/cars.csv", "./tests/expected/cars.csv"),
     ],
 )
+@mock_s3
 def test_dataset_invalid_url(url, path_to_file):
     with pytest.raises(Exception):
         read_dataset(url)

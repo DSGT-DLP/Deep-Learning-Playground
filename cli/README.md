e84c03f91dd24c24542ea39613f8b36357ace778
Daniel Wu <dwu359@gatech.edu>
2023-08-27 11:59:00 -0400

    remove cli

--- cli/README.md
+++
@@ -1,5 +0,0 @@
-### Cli Development Info
-
-Run cli in debug mode with `go run main.go`
-
-Make sure to add package paths of all files as imports in main.go
--- cli/completions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# completions.sh
-set -e
-rm -rf completions
-mkdir completions
-
-for sh in bash zsh fish; do
-	go run main.go completion "$sh" >"completions/dlp-cli.$sh"
-done

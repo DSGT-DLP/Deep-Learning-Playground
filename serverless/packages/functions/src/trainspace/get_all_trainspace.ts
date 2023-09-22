0cf2940292569b9099c6be5264dcffe91eb1a287
le_hooligan\Yvng Alan <alantao912@protonmail.ch>
2023-09-22 09:55:45 -0400

    Implemented DynamoDB built-in pagination consideration to get all trainspace endpoint

--- serverless/packages/functions/src/trainspace/get_all_trainspace.ts
+++ serverless/packages/functions/src/trainspace/get_all_trainspace.ts
@@ -11,21 +11,29 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
 
         const client: DynamoDBClient = new DynamoDBClient({});
         const docClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(client);
-        
-        const getCommand = new QueryCommand({
-            TableName: "trainspace",
-            IndexName: "uid",
-            KeyConditionExpression: "uid = :uid",
-            ExpressionAttributeValues: {
-                ":uid" : 
-                { 
-                    S: user_id
-                }
-            }
-        });
+        const fetchedTrainspaceIds: Array<string> = [];
+        let lastEvaluatedKey = undefined;
 
-        const results = await docClient.send(getCommand);
-        const fetchedTrainspaceIds = results['Items']?.map(trainspace => trainspace['trainspace_id'].S);
+        do {
+            const getCommand = new QueryCommand({
+                TableName: "trainspace",
+                IndexName: "uid",
+                KeyConditionExpression: "uid = :uid",
+                ExpressionAttributeValues: {
+                    ":uid" : 
+                    { 
+                        S: user_id
+                    }
+                },
+                ExclusiveStartKey: lastEvaluatedKey
+            });
+
+            const results = await docClient.send(getCommand);
+            lastEvaluatedKey = results.LastEvaluatedKey;
+            const page: Array<string> = results['Items']?.map(trainspace => trainspace['trainspace_id'].S);
+            page.forEach(id => fetchedTrainspaceIds.push(id));
+
+        } while (lastEvaluatedKey !== undefined);
         return { 
             statusCode: 200, 
             body: JSON.stringify({ trainspace_ids : fetchedTrainspaceIds}) 

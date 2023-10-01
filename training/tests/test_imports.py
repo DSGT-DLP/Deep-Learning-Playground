087fceef97496adac6c38ff2dbdc984304cbb826
Prisha Vallamsetti <prisha1575@gmail.com>
2023-10-01 11:28:01 -0400

    removed comment

--- training/tests/test_imports.py
+++ training/tests/test_imports.py
@@ -3,5 +3,5 @@ def test_all_imports():
     import training.core.criterion
     import training.core.optimizer
 
-    # import training.core.dl_model
+    import training.core.dl_model
     import training.core.trainer

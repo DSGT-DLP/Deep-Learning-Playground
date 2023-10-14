a9e9e4fd8530fd17e7022455fdaa17de091f0166
Prisha Vallamsetti <prisha1575@gmail.com>
2023-10-13 22:16:53 -0400

    code cleanup commit

--- backend/tests/test_model.py
+++ backend/tests/test_model.py
@@ -1,6 +1,5 @@
 import pytest
 import torch.nn as nn
-from torch.autograd import Variable
 from dl.dl_model import *
 
 
@@ -13,19 +12,7 @@ from dl.dl_model import *
     ],
 )
 def test_dlmodel(input_list):
-    print("input_list: " + str(input_list) + " is of type " + str(type(input_list)))
     my_model = DLModel(input_list)
-    print("my_model: " + str(my_model) + " is of type " + str(type(my_model)))
-    print(
-        "[module for module in my_model.model.modules() if not isinstance(module, nn.Sequential)]: "
-        + str(
-            [
-                module
-                for module in my_model.model.modules()
-                if not isinstance(module, nn.Sequential)
-            ]
-        )
-    )
     assert [
         module
         for module in my_model.model.modules()

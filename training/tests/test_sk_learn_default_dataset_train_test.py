90646335b6680ac6fb73dcd9e85dd14fae92456a
prishavall <prishavall@users.noreply.github.com>
2023-10-21 20:37:28 +0000

    :art: Format Python code with psf/black

--- training/tests/test_sk_learn_default_dataset_train_test.py
+++ training/tests/test_sk_learn_default_dataset_train_test.py
@@ -2,19 +2,21 @@ from training.core.dataset import SklearnDatasetCreator
 from torch.utils.data import DataLoader
 
 
-def test_iris_train_test_split(): 
+def test_iris_train_test_split():
     dataset_name = "IRIS"
     test_size = 0.2
     shuffle = True
 
-    dataCreator = SklearnDatasetCreator.fromDefault(
-        dataset_name, test_size, shuffle
+    dataCreator = SklearnDatasetCreator.fromDefault(dataset_name, test_size, shuffle)
+
+    X_train, X_test, y_train, y_test = (
+        dataCreator._X_train,
+        dataCreator._X_test,
+        dataCreator._y_train,
+        dataCreator._y_test,
     )
 
-    X_train, X_test, y_train, y_test = dataCreator._X_train, dataCreator._X_test, dataCreator._y_train, dataCreator._y_test
-
     total_dataset_size = len(dataCreator.getDefaultDataset(dataset_name))
-
 
     expected_size = int(total_dataset_size * test_size)
 
@@ -29,9 +31,7 @@ def test_iris_train_test_loader():
     batch_size = 20
     drop_last = True
 
-    dataCreator = SklearnDatasetCreator.fromDefault(
-        dataset_name, test_size, shuffle
-    )
+    dataCreator = SklearnDatasetCreator.fromDefault(dataset_name, test_size, shuffle)
 
     train_loader = DataLoader(
         dataCreator.createTrainDataset(),
@@ -48,21 +48,26 @@ def test_iris_train_test_loader():
     )
 
     # Check that all batches in the train loader and test loader have size batch_size and there are the right number of batches
-    X_train, X_test, y_train, y_test = dataCreator._X_train, dataCreator._X_test, dataCreator._y_train, dataCreator._y_test
+    X_train, X_test, y_train, y_test = (
+        dataCreator._X_train,
+        dataCreator._X_test,
+        dataCreator._y_train,
+        dataCreator._y_test,
+    )
 
     total_dataset_size = len(dataCreator.getDefaultDataset(dataset_name))
 
     test_len = total_dataset_size * test_size
-    train_len = total_dataset_size - test_len 
+    train_len = total_dataset_size - test_len
 
-    num_train_batches_expected =  train_len // batch_size
-    num_test_batches_expected =  test_len // batch_size
+    num_train_batches_expected = train_len // batch_size
+    num_test_batches_expected = test_len // batch_size
 
     train_batch_count = 0
     test_batch_count = 0
     # go through the train_loader. Count up how many iterations and check the batch size is matching up with batch_size
     for batch in train_loader:
-        train_features, train_labels = batch 
+        train_features, train_labels = batch
         train_batch_count += 1
         assert train_features.size()[0] == batch_size
 
@@ -70,13 +75,8 @@ def test_iris_train_test_loader():
 
     # do the same thing but for test loader
     for batch in test_loader:
-        test_features, test_labels = batch 
+        test_features, test_labels = batch
         test_batch_count += 1
         assert test_features.size()[0] == batch_size
-    
+
     assert test_batch_count == num_test_batches_expected
-    
-
-
-
-

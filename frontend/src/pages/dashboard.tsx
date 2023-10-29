4d38a89ba681f1c4e7ef09bd6b1113a4dafdd6f4
Daniel Wu <dwu359@gatech.edu>
2023-10-29 18:24:17 -0400

    workflow

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
   useEffect(() => {
     if (router.isReady && !user) {
       router.replace({ pathname: "/login" });
-      console.log("remove me2");
+      console.log("remove me3");
     }
   }, [user, router.isReady]);
   if (!isSignedIn(user)) {

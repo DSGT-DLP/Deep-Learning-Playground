73c1fac84a2338177022b44f41db6788ca3fae3d
le_hooligan\Yvng Alan <alantao912@protonmail.ch>
2023-11-19 17:36:34 -0500

    Fixed name format

--- frontend/src/common/utils/nameFormat.ts
+++ frontend/src/common/utils/nameFormat.ts
@@ -1,3 +1,10 @@
 export const isNameValid = (name: string) : boolean => {
-    return /[^a-zA-Z]/.test(name)
+    name = name.toLowerCase();
+    for (let i = 0; i < name.length; ++i) {
+        const c = name.charAt(i);
+        if (c.toLowerCase() === c.toUpperCase()) {
+            return false;
+        }
+    }
+    return true;
 }

46c482d8a0cc2f07e5e4ea39ee4c30eb5d23315e
karkir0003 <karkir0003@gmail.com>
2023-11-23 21:39:20 -0500

    unit test for /columns django endpoint

---
+++ training/tests/utils/test_utils.py
@@ -0,0 +1,30 @@
+"""
+File that houses helper functions for testing the training backend 
+"""
+import jwt
+import datetime
+
+def mock_authenticate(*args, **kwargs) -> str:
+    """
+    Function that gives a test JWT Token for testing (not necessarily real user data)
+    Django API Endpoints that require user authentication
+
+    Returns:
+        token: Bearer Token
+    """
+    payload = {
+        "sub": "1234567890",
+        "name": "John Doe",
+        "iat": datetime.datetime.utcnow(),
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+    }
+    secret = 'secret' 
+    token = jwt.encode(payload, secret, algorithm='HS256')
+    return token
+
+def get_test_bearer_token() -> dict:
+    """
+    Wrapper that uses mock_authenticate function to build a bearer token 
+    in a format that Django accepts
+    """
+    return {'HTTP_AUTHORIZATION': 'Bearer ' + mock_authenticate()}

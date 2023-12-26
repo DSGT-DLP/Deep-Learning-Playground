32cd089a8d52d23344b96e203c4d615f7e4318c2
hartejsingh12 <12.singh.hartej@gmail.com>
2023-12-26 15:41:12 -0500

    Intialized branch for progress

--- frontend/src/common/components/Footer.tsx
+++ frontend/src/common/components/Footer.tsx
@@ -3,8 +3,6 @@ import { COLORS, URLs } from "../../constants";
 import { LinkedIn, YouTube, Instagram, GitHub } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-//Comment for initial commit (Github)
-
 const theme = createTheme({
   components: {
     MuiSvgIcon: {

052f3b53b30433bdf27a07e9a8e61b6f94718107
hartejsingh12 <12.singh.hartej@gmail.com>
2023-12-29 21:28:30 -0500

    Added a sidebar component to the trainspace page
    Specifically, modified the trainspace.tsx page to display a sidebar component with a fixed width and a responsiveheight. Test cases will be added later for the responsive height.

--- frontend/src/pages/trainspace.tsx
+++ frontend/src/pages/trainspace.tsx
@@ -3,11 +3,24 @@ import React from "react";
 
 const TrainSpace = () => {
     return (
-      <div>
-        <NavbarMain />
-        <h1>Welcome to TrainSpace</h1>
-      </div>
+        <div>
+            <NavbarMain />
+            <div style={{ display: 'flex' }}>
+                <div style={{
+                    width: '200px',
+                    height: 'calc(100vh - 60px)',
+                    position: 'fixed',
+                    backgroundColor: '#f5f5f5',
+                    top: '64px'
+                }}>
+                    <p>Sidebar content will go here</p>
+                </div>
+                <div style={{ marginLeft: '200px' }}>
+                    <h1>Welcome to TrainSpace</h1>
+                </div>
+            </div>
+        </div>
     );
-  };
-  
-  export default TrainSpace;
+};
+
+export default TrainSpace;

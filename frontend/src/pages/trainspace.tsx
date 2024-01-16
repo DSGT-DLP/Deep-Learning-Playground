6315ca959860b059e1321f2ab1ffede1843da497
hartejsingh12 <12.singh.hartej@gmail.com>
2024-01-15 21:53:59 -0500

    other

--- frontend/src/pages/trainspace.tsx
+++ frontend/src/pages/trainspace.tsx
@@ -5,7 +5,7 @@ const TrainSpace = () => {
     return (
         <div>
             <NavbarMain />
-            //Menubar
+            //Menuba
             <div style={{ 
                 width: '100%', 
                 height: '50px', 

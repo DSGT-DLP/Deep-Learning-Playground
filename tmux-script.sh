05f6406d91640325cf88e90f3d4b851054b29122
Xiaomeng Ye <yexiaomeng2004@gmail.com>
2024-02-20 19:19:23 -0500

    tmux script updated to use dlp-cli (#1088)
    * updating tmux script to use dlp-cli
    
    * :art: Auto-generated directory tree for repository in Architecture.md
    
    ---------
    
    Co-authored-by: Xiaomeng Ye <yebyyy@lawn-128-61-121-187.lawn.gatech.edu>
    Co-authored-by: Keon Sanavandi <keonsanavandijoon@gmail.com>
    Co-authored-by: Keon-San <Keon-San@users.noreply.github.com>

--- tmux-script.sh
+++ tmux-script.sh
@@ -2,7 +2,7 @@
 session="dl-playground"
 tmux new-session -d -s $session
 tmux rename-window -t 0 'backend'
-tmux send-keys -t 'backend' 'yarn run startt' C-m
+tmux send-keys -t 'backend' 'dlp-cli backend start' C-m
 tmux new-window -t $session:1 -n 'frontend'
-tmux send-keys -t $session:1 'yarn run startf' C-m
+tmux send-keys -t $session:1 'dlp-cli frontend start' C-m
 tmux attach-session -t $session:0

1190f83ad3ba54b2fb06862c71b6272a13f64f62
prishavall <prisha1575@gmail.com>
2024-03-08 22:34:35 -0500

    checking the frontend workflow unit test after fixing lint

--- frontend/src/__tests__/common/components/TitleText.test.tsx
+++ frontend/src/__tests__/common/components/TitleText.test.tsx
@@ -1,13 +1,13 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import TitleText from '@/common/components/TitleText'
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import TitleText from '@/common/components/TitleText';
  
 describe('Page', () => {
   it('renders a heading', () => {
-    render(<TitleText text="Test Title Text"/>)
+    render(<TitleText text="Test Title Text"/>);
  
-    const heading = screen.getByRole('heading', { level: 2 })
+    const heading = screen.getByRole('heading', { level: 2 });
  
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
